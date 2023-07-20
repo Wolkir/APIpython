a2eec3fa463dc7c2f13354a41f9963af502ba1bf
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-21 00:31:57 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -6,7 +6,6 @@ import bcrypt
 # Connexion à la base de données MongoDB
 client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
-trade_collection = db["things"]
 
 app = Flask(__name__)
 
@@ -20,6 +19,7 @@ def save_trade_request():
     data = request.json
     username = data.get('username')
     password = data.get('password')
+    closure_position = data.get('closurePosition')
 
     try:
         # Vérifier l'authentification de l'utilisateur
@@ -29,6 +29,12 @@ def save_trade_request():
 
         # Hacher le mot de passe
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        # Construire le nom de la collection en fonction de closurePosition
+        collection_name = f"{username}_open" if closure_position == "Open" else f"{username}_close"
+
+        # Récupérer la collection correspondant au nom d'utilisateur et closurePosition
+        user_collection = db[collection_name]
 
         # Créer une nouvelle instance de TradeRequest à partir des données reçues
         trade_request = {
@@ -40,7 +46,7 @@ def save_trade_request():
             "dateAndTimeOpening": data.get('dateAndTimeOpening'),
             "typeOfTransaction": data.get('typeOfTransaction'),
             "volume": data.get('volume'),
-            "symbole": data.get('symbole'),
+            "symbol": data.get('symbole'),
             "priceOpening": data.get('priceOpening'),
             "stopLoss": data.get('stopLoss'),
             "takeProfit": data.get('takeProfit'),
@@ -48,14 +54,17 @@ def save_trade_request():
             "priceClosure": data.get('priceClosure'),
             "swap": data.get('swap'),
             "profit": data.get('profit'),
-            "commission": data.get('commission'),
-            "closurePosition": data.get('closurePosition'),
+            "commission": data.get('commision'),
+            "closurePosition": closure_position,
             "balance": data.get('balance')
             # Ajoutez ici les autres champs de la demande de transaction en fonction de vos besoins
         }
 
-        # Enregistrer l'objet dans la base de données
-        trade_collection.insert_one(trade_request)
+        # Enregistrer l'objet dans la collection de l'utilisateur et closurePosition
+        user_collection.insert_one(trade_request)
         return jsonify({"message": "Data saved successfully Python"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+# Enregistrement du blueprint dans l'application Flask
+app.register_blueprint(trade_blueprint, url_prefix='/api')
