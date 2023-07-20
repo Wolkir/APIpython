9c64dca73f2ad0a33df2fc87dc6f36009fc432e7
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-20 17:07:13 +0100

    m

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 from connexion.user.login import setup_login_routes
 from connexion.user.signup import setup_signup_route
 
-#from connexion.tradReq import trade_blueprint
+# from connexion.tradReq import trade_blueprint
 from connexion.tradReq.tradReq import trade_blueprint
 
 from connexion.strategie.createStrategie import createStrategie
@@ -46,66 +46,25 @@ from routes.calcul.conversion_map import conversion_map
 from routes.envoie.envoie import envoie
 
 app = Flask(__name__)
+
+# Fonction pour gérer les en-têtes CORS
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    return response
+
+# Appliquer le décorateur after_request à toutes les réponses
+app.after_request(after_request)
+
 CORS(app)
-CORS(app, origins='*')
 
 # user
 app.register_blueprint(setup_signup_route(app))
 app.register_blueprint(setup_login_routes(app))
 app.register_blueprint(trade_blueprint)
-"""
-app.register_blueprint(login)
-app.register_blueprint(signup)
-app.register_blueprint(createStrategie)
-app.register_blueprint(suppressionStrategie)
-app.register_blueprint(recuperationStrategie)
-app.register_blueprint(trade_blueprint)
-
-app.register_blueprint(strategie_blueprint)
-
-# date
-app.register_blueprint(dateDuJour)
-app.register_blueprint(semaineGlissante)
-app.register_blueprint(semaineEnCours)
-app.register_blueprint(moisEnCours)
-app.register_blueprint(moisGlissant)
-app.register_blueprint(Symbole)
-"""
-# calcul
-app.register_blueprint(RR)
-app.register_blueprint(BE)
-app.register_blueprint(Equity)
-app.register_blueprint(tpr)
-app.register_blueprint(slr)
-app.register_blueprint(Tilts)
-app.register_blueprint(profitfactor)
-app.register_blueprint(profitfactorlong)
-app.register_blueprint(profitfactorshort)
-app.register_blueprint(winrate)
-app.register_blueprint(averagegain)
-app.register_blueprint(averageloss)
-app.register_blueprint(average_duration)
-app.register_blueprint(maxprofit)
-app.register_blueprint(minloss)
-app.register_blueprint(calculate_duration)
-app.register_blueprint(ddmax)
-app.register_blueprint(session)
-app.register_blueprint(sharp_ratio)
-app.register_blueprint(killzone)
-app.register_blueprint(weekday)
-app.register_blueprint(tradecount)
-app.register_blueprint(assign_order)
-app.register_blueprint(average_rr)
-# app.register_blueprint(conversion_map)
-
-# envoie
-app.register_blueprint(envoie)
-
-#app.register_blueprint(Symbole)
-
-@app.route('/')
-def hello_world():
-    return 'reussi en depit de ents'
+# Autres routes...
+# ...
 
 if __name__ == '__main__':
     url = "mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority"
