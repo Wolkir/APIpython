49039341414231d56cf49a71f9bcab2576fb4ad4
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-20 14:37:15 +0100

    m

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -1,18 +1,19 @@
 from flask import Flask, Blueprint, jsonify, request
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import bcrypt
 
-# Création de l'application Flask
-app = Flask(__name__)
+# Connexion à la base de données MongoDB
+client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
+db = client["test"]
+trade_collection = db["things"]
 
-# Configuration de la connexion MongoDB
-app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-mongo = PyMongo(app)
+app = Flask(__name__)
 
 trade_blueprint = Blueprint('trade', __name__)
 
 def compare_passwords(password, hashed_password):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 @trade_blueprint.route('/savetraderequest', methods=['POST'])
 def save_trade_request():
@@ -21,15 +22,12 @@ def save_trade_request():
     password = data.get('password')
 
     try:
-        # Connexion à la base de données MongoDB (déjà initialisée)
-        global mongo  # Indiquer que l'on souhaite utiliser la variable 'mongo' globale
-
         # Vérifier l'authentification de l'utilisateur
-        user = mongo.db.users.find_one({"username": username})
+        user = db.users.find_one({"username": username})
         if not user or not compare_passwords(password, user['password']):
-            return jsonify({"message": "Authentification échouée"}), 401
+            return jsonify({"message": "Access denied"}), 401
 
-        # Hacher le mot de passe avant de l'enregistrer dans la collection traderequests
+        # Hacher le mot de passe
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         # Créer une nouvelle instance de TradeRequest à partir des données reçues
@@ -42,7 +40,7 @@ def save_trade_request():
             "dateAndTimeOpening": data.get('dateAndTimeOpening'),
             "typeOfTransaction": data.get('typeOfTransaction'),
             "volume": data.get('volume'),
-            "symbol": data.get('symbol'),  # Corrected the spelling of 'symbol'
+            "symbol": data.get('symbol'),
             "priceOpening": data.get('priceOpening'),
             "stopLoss": data.get('stopLoss'),
             "takeProfit": data.get('takeProfit'),
@@ -50,14 +48,14 @@ def save_trade_request():
             "priceClosure": data.get('priceClosure'),
             "swap": data.get('swap'),
             "profit": data.get('profit'),
-            "commission": data.get('commission'),  # Corrected the spelling of 'commission'
+            "commission": data.get('commission'),
             "closurePosition": data.get('closurePosition'),
             "balance": data.get('balance')
             # Ajoutez ici les autres champs de la demande de transaction en fonction de vos besoins
         }
 
         # Enregistrer l'objet dans la base de données
-        mongo.db.thingsTest.insert_one(trade_request)
-        return jsonify({"message": "Données enregistrées avec succès"}), 201
+        trade_collection.insert_one(trade_request)
+        return jsonify({"message": "Data saved successfully"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
