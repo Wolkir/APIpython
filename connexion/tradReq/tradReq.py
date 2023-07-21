5cf2ff028ca3fd318732bb0f50bc629a728caff0
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-21 09:29:16 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -1,19 +1,3 @@
-from flask import Flask, Blueprint, jsonify, request
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-import bcrypt
-
-# Connexion à la base de données MongoDB
-client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
-
-app = Flask(__name__)
-
-trade_blueprint = Blueprint('trade', __name__)
-
-def compare_passwords(password, hashed_password):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
 @trade_blueprint.route('/savetraderequest', methods=['POST'])
 def save_trade_request():
     data = request.json
@@ -63,22 +47,26 @@ def save_trade_request():
                 user_collection.insert_one(trade_request)
         else:
             # Si l'ordre est une demande de fermeture, nous devons vérifier s'il correspond à un ordre ouvert existant
-            collection_name = f"{username}_open"
-            user_collection = db[collection_name]
+            collection_name_open = f"{username}_open"
+            collection_name_close = f"{username}_close"
+            user_collection_open = db[collection_name_open]
+            user_collection_close = db[collection_name_close]
 
-            existing_open_order = user_collection.find_one({"identifier": data.get('identifier'), "closurePosition": "Open"})
+            existing_open_order = user_collection_open.find_one({"identifier": data.get('identifier'), "closurePosition": "Open"})
 
             if existing_open_order:
                 # Mettre à jour le volume fermé pour l'ordre ouvert
                 existing_open_order['volume_closed'] = existing_open_order.get('volume_closed', 0) + data.get('volume')
 
-                # Vérifier si le volume fermé correspond au volume total de l'ordre ouvert
                 if existing_open_order['volume_closed'] >= existing_open_order['volume']:
+                    # L'ordre ouvert est entièrement fermé, donc nous l'enregistrons dans la collection des ordres fermés
+                    user_collection_close.insert_one(existing_open_order)
+
                     # Supprimer l'ordre ouvert de la collection des ordres ouverts
-                    user_collection.delete_one({"_id": existing_open_order["_id"]})
+                    user_collection_open.delete_one({"_id": existing_open_order["_id"]})
                 else:
                     # Mettre à jour l'ordre ouvert avec le nouveau volume fermé
-                    user_collection.replace_one({"_id": existing_open_order["_id"]}, existing_open_order)
+                    user_collection_open.replace_one({"_id": existing_open_order["_id"]}, existing_open_order)
             else:
                 # Si l'ordre ouvert n'existe pas, nous ne pouvons pas fermer cet ordre
                 return jsonify({"message": "Open order not found"}), 404
@@ -86,6 +74,3 @@ def save_trade_request():
         return jsonify({"message": "Data saved successfully"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-# Enregistrement du blueprint dans l'application Flask
-app.register_blueprint(trade_blueprint, url_prefix='/api')
