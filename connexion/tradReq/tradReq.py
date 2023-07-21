f27517a50e0668ad07329f9db412d66c883043d9
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-22 00:11:52 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -11,11 +11,74 @@ app = Flask(__name__)
 
 trade_blueprint = Blueprint('trade', __name__)
 
-# Define the compare_passwords function
 def compare_passwords(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
 
-@trade_blueprint.route('/savetraderequest', methods=['POST'])
+def handle_close_transaction(username, data):
+    # Récupérer la collection correspondant aux positions ouvertes de l'utilisateur
+    open_collection = db[f"{username}_open"]
+
+    # Chercher si une position ouverte avec le même identifier et volume existe
+    existing_position = open_collection.find_one({
+        "identifier": data.get('identifier'),
+        "volume": data.get('volume'),
+    })
+
+    if existing_position:
+        # S'il y a une position ouverte avec le même identifier et volume
+        if existing_position['volume'] == data.get('volume'):
+            # Supprimer la position ouverte
+            open_collection.delete_one({"_id": existing_position["_id"]})
+        else:
+            # La position ouverte doit être mise à jour avec le nouveau volume
+            updated_volume = existing_position['volume'] - data.get('volume')
+            open_collection.update_one(
+                {"_id": existing_position["_id"]},
+                {"$set": {"volume": updated_volume}}
+            )
+
+    # Récupérer la collection correspondant aux positions fermées de l'utilisateur
+    close_collection = db[f"{username}_close"]
+
+    # Chercher si une position fermée avec le même identifier existe
+    existing_close_position = close_collection.find_one({
+        "identifier": data.get('identifier'),
+    })
+
+    if existing_close_position:
+        # Une position fermée avec le même identifier existe
+        # Ajouter le volume à la position existante
+        updated_volume = existing_close_position['volume'] + data.get('volume')
+        close_collection.update_one(
+            {"_id": existing_close_position["_id"]},
+            {"$set": {"volume": updated_volume}}
+        )
+    else:
+        # Aucune position fermée avec le même identifier, créer une nouvelle
+        trade_request = {
+            "username": username,
+            "ticketNumber": data.get('ticketNumber'),
+            "identifier": data.get('identifier'),
+            "magicNumber": data.get('magicNumber'),
+            "dateAndTimeOpening": data.get('dateAndTimeOpening'),
+            "typeOfTransaction": data.get('typeOfTransaction'),
+            "volume": data.get('volume'),
+            "symbol": data.get('symbole'),
+            "priceOpening": data.get('priceOpening'),
+            "stopLoss": data.get('stopLoss'),
+            "takeProfit": data.get('takeProfit'),
+            "dateAndTimeClosure": data.get('dateAndTimeClosure'),
+            "priceClosure": data.get('priceClosure'),
+            "swap": data.get('swap'),
+            "profit": data.get('profit'),
+            "commission": data.get('commision'),
+            "balance": data.get('balance'),
+            "broker": data.get('broker')
+            # Ajoutez ici les autres champs de la demande de transaction en fonction de vos besoins
+        }
+        close_collection.insert_one(trade_request)
+
+# Route pour enregistrer une transaction
 @trade_blueprint.route('/savetraderequest', methods=['POST'])
 def save_trade_request():
     data = request.json
@@ -32,67 +95,45 @@ def save_trade_request():
         # Hacher le mot de passe
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        # Construire le nom de la collection en fonction de closurePosition
-        collection_name = f"{username}_open" if closure_position == "Open" else f"{username}_close"
+        if closure_position == "Open":
+            # Construire le nom de la collection pour les positions ouvertes
+            collection_name = f"{username}_open"
+            # Récupérer la collection correspondant aux positions ouvertes de l'utilisateur
+            user_collection = db[collection_name]
 
-        # Récupérer la collection correspondant au nom d'utilisateur et closurePosition
-        user_collection = db[collection_name]
-
-        # Chercher si une position ouverte avec le même identifier et volume existe
-        existing_position = None
-        if closure_position == "Close":
-            existing_position = user_collection.find_one({
+            # Créer une nouvelle instance de TradeRequest à partir des données reçues
+            trade_request = {
+                "username": username,
+                "password": hashed_password,
+                "ticketNumber": data.get('ticketNumber'),
                 "identifier": data.get('identifier'),
+                "magicNumber": data.get('magicNumber'),
+                "dateAndTimeOpening": data.get('dateAndTimeOpening'),
+                "typeOfTransaction": data.get('typeOfTransaction'),
                 "volume": data.get('volume'),
-            })
+                "symbol": data.get('symbole'),
+                "priceOpening": data.get('priceOpening'),
+                "stopLoss": data.get('stopLoss'),
+                "takeProfit": data.get('takeProfit'),
+                "closurePosition": closure_position,
+                "balance": data.get('balance'),
+                "broker": data.get('broker')
+                # Ajoutez ici les autres champs de la demande de transaction en fonction de vos besoins
+            }
 
-        if existing_position:
-            # S'il y a une position ouverte avec le même identifier et volume
-            if existing_position['volume'] == data.get('volume'):
-                # Supprimer la position ouverte
-                user_collection.delete_one({"_id": existing_position["_id"]})
-            else:
-                # La position ouverte doit être mise à jour avec le nouveau volume
-                updated_volume = existing_position['volume'] - data.get('volume')
-                user_collection.update_one(
-                    {"_id": existing_position["_id"]},
-                    {"$set": {"volume": updated_volume}}
-                )
+            # Enregistrer l'objet dans la collection de l'utilisateur et closurePosition
+            user_collection.insert_one(trade_request)
 
-        # Créer une nouvelle instance de TradeRequest à partir des données reçues
-        trade_request = {
-            "username": username,
-            "password": hashed_password,
-            "ticketNumber": data.get('ticketNumber'),
-            "identifier": data.get('identifier'),
-            "magicNumber": data.get('magicNumber'),
-            "dateAndTimeOpening": data.get('dateAndTimeOpening'),
-            "typeOfTransaction": data.get('typeOfTransaction'),
-            "volume": data.get('volume'),
-            "symbol": data.get('symbole'),
-            "priceOpening": data.get('priceOpening'),
-            "stopLoss": data.get('stopLoss'),
-            "takeProfit": data.get('takeProfit'),
-            "dateAndTimeClosure": data.get('dateAndTimeClosure'),
-            "priceClosure": data.get('priceClosure'),
-            "swap": data.get('swap'),
-            "profit": data.get('profit'),
-            "commission": data.get('commision'),
-            "closurePosition": closure_position,
-            "balance": data.get('balance'),
-            "broker": data.get('broker')
-            # Ajoutez ici les autres champs de la demande de transaction en fonction de vos besoins
-        }
+        elif closure_position == "Close":
+            handle_close_transaction(username, data)
 
-        # Enregistrer l'objet dans la collection de l'utilisateur et closurePosition
-        user_collection.insert_one(trade_request)
-
-        return jsonify({"message": "Data saved successfully Python CLOSE"}), 201
+        return jsonify({"message": "Data saved successfully"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
 # Enregistrement du blueprint dans l'application Flask
 app.register_blueprint(trade_blueprint, url_prefix='/api')
 
+# Lancer l'application Flask
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
