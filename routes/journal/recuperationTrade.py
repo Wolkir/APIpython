96eb9d707e0d63fd353ca5b3e34e73310cc352e6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-23 16:15:11 +0100

    m

--- routes/journal/recuperationTrade.py
+++ routes/journal/recuperationTrade.py
@@ -1,8 +1,19 @@
 from flask import Blueprint, jsonify, request
 from flask_pymongo import PyMongo
 import jwt
+from bson import ObjectId
 
 things_blueprint = Blueprint('things', __name__)
+
+def convert_to_json_serializable(data):
+    for key, value in data.items():
+        if isinstance(value, bytes):
+            data[key] = str(value)
+        elif isinstance(value, ObjectId):
+            data[key] = str(value)
+        elif isinstance(value, dict):
+            data[key] = convert_to_json_serializable(value)
+    return data
 
 def setup_things_routes(app):
     @things_blueprint.route('/recuperationTrade', methods=['GET'])
@@ -15,7 +26,7 @@ def setup_things_routes(app):
             all_things = list(things_collection.find({}))
 
             for thing in all_things:
-                thing['_id'] = str(thing['_id'])
+                thing = convert_to_json_serializable(thing)
 
             return jsonify(all_things), 200
 
