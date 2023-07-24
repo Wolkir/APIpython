7afacbbda5ee1612eb1e35dfd5707b2a2c9cd413
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-25 00:00:00 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -67,7 +67,7 @@ def save_trade_request():
             "magicNumber": data.get('magicNumber'),
             "dateAndTimeOpening": data.get('dateAndTimeOpening'),
             "typeOfTransaction": data.get('typeOfTransaction'),
-            "orderType":data.get('orderType')
+            "orderType":data.get('orderType'),
             "volume": data.get('volume'),
             "volume_remain": volume_remain,
             "symbol": data.get('symbole'),
