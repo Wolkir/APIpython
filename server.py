c2d069a306d2b0aa74df2d6e721abc3b85e762c1
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-24 14:55:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ from routes.calcul.tradecount import tradecount
 from routes.calcul.assign_order import assign_order
 from routes.calcul.average.average_rr import average_rr
 from routes.calcul.conversion_map import conversion_map
-
+from routes.calcul.test import test
 # envoie
 from routes.envoie.envoie import envoie
 
