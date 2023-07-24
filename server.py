7494bd0a09134aed1a7924b8017cce9145f61913
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-24 14:57:24 +0200

    Update server.py

--- server.py
+++ server.py
@@ -66,6 +66,7 @@ app.register_blueprint(setup_login_routes(app))
 app.register_blueprint(setup_user_routes(app))
 
 app.register_blueprint(trade_blueprint)
+app.register_blueprint(test)
 
 app.register_blueprint(setup_createStrategie_routes(app))
 app.register_blueprint(setup_recuperationStrategie_routes(app))
