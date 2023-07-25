bd813de9a35454d2e4b6d8aca3fb1acc01ad8816
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-25 15:45:54 +0100

    Update recuperationTrade.py

--- routes/journal/recuperationTrade.py
+++ routes/journal/recuperationTrade.py
@@ -17,7 +17,7 @@ def convert_to_json_serializable(data):
         elif isinstance(value, dict):
             data[key] = convert_to_json_serializable(value)
     return data
-"""
+
 def setup_things_routes(app):
     @things_blueprint.route('/recuperationTrade', methods=['GET'])
     def get_all_things():
@@ -49,45 +49,5 @@ def setup_things_routes(app):
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-
-    @things_blueprint.route('/updateTrade', methods=['POST'])
-    def update_trade():
-        try:
-            app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-            mongo = PyMongo(app)
-            logging.debug("connexion mongo établie")
-            data = request.get_json()
-            trades_data = data.get('trades', [])
-            psychologie_data = data.get('psychologie', [])
-            logging.debug("tableaux récupérés")
-            things_collection = mongo.db.things
-
-            # Mise à jour ou création des champs psychologie
-            for psychologie_item in psychologie_data:
-                trade_id = psychologie_item.get('id')
-                value_psy = psychologie_item.get('valuePsy')
-                logging.debug("récupération trade_id et valuePsy")
-
-                if trade_id and value_psy:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'psychologie': value_psy}}, upsert=True)
-            
-            # Mise à jour du champ annonceEconomique
-            for trade in trades_data:
-                trade_id = trade.get('id')
-                valeur_ann_eco = trade.get('valeurAnnEco')
-                logging.debug("récupération trade_id et valeur_ann_eco")
-
-                if trade_id and valeur_ann_eco in ['oui', 'non']:
-                    annonce_economique = True if valeur_ann_eco == 'oui' else False
-                    logging.debug("conversion true false")
-
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'annonceEconomique': annonce_economique}})
-
-            return jsonify({"message": "Trade details updated successfully."}), 200
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-
+        
     return things_blueprint
-
-"""
