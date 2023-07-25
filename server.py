947f68327b1a3cacf7534ce7fa514e5256bc0ff3
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-25 16:29:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ app.register_blueprint(setup_user_routes(app))
 #app.register_blueprint(trade_blueprint)
 
 #journal
-app.register_blueprint(setup_things_routes(app))
+#app.register_blueprint(setup_things_routes(app))
 app.register_blueprint(modificationTrade)
 
 #===========================================LANCEMENT DU SERVER===============================================#
