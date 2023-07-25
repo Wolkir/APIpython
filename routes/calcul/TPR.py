ed3bbaa264c6bcc0be417cfc0b449d9073ea7f25
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-25 11:51:34 +0200

    Update TPR.py

--- routes/calcul/TPR.py
+++ routes/calcul/TPR.py
@@ -14,7 +14,7 @@ tpr = Blueprint('tpr', __name__)
 def update_tpr():
     client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
     db = client['test']
-    collection = db['test2_close']
+    collection = db['test2_open']
 
     data = list(collection.find())
 
