21f1eef000d1de6d23ef4aab55036c71d9b44e02
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 00:58:49 +0200

    Update TPR.py

--- routes/calcul/TPR.py
+++ routes/calcul/TPR.py
@@ -4,38 +4,39 @@ from pymongo import MongoClient
 # Connexion à la base de données MongoDB
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 
-slr = Blueprint('slr', __name__)
+tpr = Blueprint('tpr', __name__)
 
-def calculate_slr(entry):
-    # Your SLR calculation logic here based on the 'entry' data
+def calculate_tpr(entry):
+    # Your TPR calculation logic here based on the 'entry' data
     # For example:
     type_of_transaction = entry.get('typeOfTransaction')
     price_closure = entry.get('priceClosure')
-    stop_loss = entry.get('stopLoss')
-
-    if type_of_transaction == "buy" and price_closure <= stop_loss:
-        entry['SLR'] = True
-    elif type_of_transaction == "sell" and price_closure >= stop_loss:
-        entry['SLR'] = True
+    take_profit = entry.get('takeProfit')
+    
+    if type_of_transaction == "buy" and price_closure >= take_profit:
+        entry['TPR'] = True
+    elif type_of_transaction == "sell" and price_closure <= take_profit:
+        entry['TPR'] = True
     else:
-        entry['SLR'] = False
+        entry['TPR'] = False
 
     return entry
 
-@slr.route('/slr', methods=['POST'])
-def update_slr():
+
+@tpr.route('/tpr', methods=['POST'])
+def update_tpr():
     try:
         data = request.json
 
         for entry in data:
-            # Calculate the SLR value for each entry
-            entry = calculate_slr(entry)
+            # Calculate the TPR value for each entry
+            entry = calculate_tpr(entry)
 
-            # Update the entry in the database with the SLR value
+            # Update the entry in the database with the TPR value
             db = client['test']
             collection = db['test2_open']
-            collection.update_one({'_id': entry['_id']}, {'$set': {'SLR': entry['SLR']}})
+            collection.update_one({'_id': entry['_id']}, {'$set': {'TPR': entry['TPR']}})
         
-        return jsonify({'message': 'SLR updated successfully'}), 200
+        return jsonify({'message': 'TPR updated successfully'}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
