69a484b10fb76f0cf383b38b35860fb29dd6fc32
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 01:27:11 +0200

    Update calculate_duration.py

--- routes/calcul/calculate_duration.py
+++ routes/calcul/calculate_duration.py
@@ -1,34 +1,17 @@
-from flask import Flask, Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from datetime import datetime
 
 calculate_duration = Blueprint('calculate_duration', __name__)
 
 def calculate_time_duration(data):
     try:
-        # Calculer la durée pour chaque document dans les données JSON
-        for doc in data:
-            opening_time = datetime.fromisoformat(doc['dateAndTimeOpening'])
-            closure_time = datetime.fromisoformat(doc['dateAndTimeClosure'])
-            
-            # Calculer la durée
+        # Calculate the duration for each data entry in the list
+        for entry in data:
+            opening_time = datetime.fromisoformat(entry['dateAndTimeOpening'])
+            closure_time = datetime.fromisoformat(entry['dateAndTimeClosure'])
             duration = closure_time - opening_time
-            doc['duration'] = str(duration)
+            entry['duration'] = str(duration)
         
-        return data, 200
-    except Exception as e:
-        return {"error": str(e)}, 400
-
-@calculate_duration.route('/calculate_duration', methods=['POST'])
-def update_time_duration():
-    try:
-        data = request.json
-        # Call the calculate_time_duration() function and pass the JSON data
-        updated_data, status_code = calculate_time_duration(data)
-        return jsonify(updated_data), status_code
+        return jsonify({"message": "La clé 'durée' a été ajoutée aux données JSON"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.register_blueprint(calculate_duration, url_prefix='/api')
-    app.run()
