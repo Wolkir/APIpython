e7c3fce9dd7dadc2da22a22f7961c9006c75fc7c
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 00:37:12 +0200

    Create SLR.py

---
+++ routes/calcul/SLR.py
@@ -0,0 +1,41 @@
+# Code SLR
+
+slr = Blueprint('slr', __name__)
+
+def calculate_slr(entry):
+    type_of_transaction = entry.get('typeOfTransaction')
+    price_closure = entry.get('priceClosure')
+    stop_loss = entry.get('stopLoss')
+
+    if type_of_transaction == "buy" and price_closure <= stop_loss:
+        entry['SLR'] = True
+    elif type_of_transaction == "sell" and price_closure >= stop_loss:
+        entry['SLR'] = True
+    else:
+        entry['SLR'] = False
+
+    return entry
+
+@slr.route('/slr', methods=['POST'])
+def update_slr():
+    try:
+        data = request.json
+
+        for entry in data:
+            # Calculate the SLR value for each entry
+            entry = calculate_slr(entry)
+
+            # Update the entry in the database with the SLR value
+            db = client['test']
+            collection = db['test2_open']
+            collection.update_one({'_id': entry['_id']}, {'$set': {'SLR': entry['SLR']}})
+        
+        return jsonify({'message': 'SLR updated successfully'}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+# Enregistrement du blueprint "slr" dans l'application Flask
+app.register_blueprint(slr, url_prefix='/slr')
+
+if __name__ == '__main__':
+    app.run()
