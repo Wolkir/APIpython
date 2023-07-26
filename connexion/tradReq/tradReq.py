bfc936a285fd435f556d5615a4420b55dae0cbed
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 00:12:28 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -75,12 +75,11 @@ def save_trade_request():
             session = determine_session(data)
             data['session'] = session
 
-            #duration = calculate_time_duration(data)
-            #data['duration'] = duration
-            calculate_time_duration(data)
+            duration = calculate_time_duration(data)
+            data['duration'] = duration
+       
 
-            # Get the 'duration' for the first entry (assuming it's the same for all entries)
-            duration = data[0]['duration'
+          
 
         # Round 'volume' and 'volume_remain' to two decimal places
         data['volume'] = round(data.get('volume'), 2)
