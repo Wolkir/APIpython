69861c77871a247cbedd4f851fbd6291950f3bd1
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 13:24:15 +0200

    Update killzone.py

--- routes/calcul/killzone.py
+++ routes/calcul/killzone.py
@@ -3,7 +3,7 @@ from datetime import time
 
 #killzone_blueprint = Blueprint('killzone', __name__)
 killzone = Blueprint('killzone', __name__)
-  data = request.json
+data = request.json
 #@killzone_blueprint.route('/killzone', methods=['POST'])
 def calculate_killzone(data):
   
@@ -12,7 +12,7 @@ def calculate_killzone(data):
     # Parcourir tous les documents de la collection
     for doc in data:
         # Récupérer l'heure d'ouverture de chaque document
-        opening_time = datetime.strptime(doc['dateAndTimeOpening'], "%Y.%m.%d %H:%M:%S.%f").time()
+        opening_time = datetime.strptime(data['dateAndTimeOpening'], "%Y.%m.%d %H:%M:%S.%f").time()
 
         # Déterminer si l'heure d'ouverture se trouve dans l'une des plages horaires spécifiées
         if (time(3, 0) <= opening_time <= time(6, 0)) or (time(9, 0) <= opening_time <= time(12, 0)) or (time(14, 0) <= opening_time <= time(17, 0)):
