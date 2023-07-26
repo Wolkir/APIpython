02a2c30911fda6c2687adfae04119fa4e14b8350
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 12:53:18 +0200

    Update killzone.py

--- routes/calcul/killzone.py
+++ routes/calcul/killzone.py
@@ -1,23 +1,24 @@
-from flask import Blueprint, jsonify, request
+from flask import Flask, Blueprint, jsonify
 from pymongo import MongoClient
+from datetime import datetime, time
 
 # Connexion à la base de données MongoDB
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 
-tpr = Blueprint('tpr', __name__)
+killzone = Blueprint('killzone', __name__)
 
-def calculate_tpr(entry):
-    # Your TPR calculation logic here based on the 'entry' data
-    # For example:
-    type_of_transaction = entry.get('typeOfTransaction')
-    price_closure = entry.get('priceClosure')
-    take_profit = entry.get('takeProfit')
-    
-    if type_of_transaction == "Buy" and price_closure >= take_profit:
-        entry['TPR'] = True
-    elif type_of_transaction == "Sell" and price_closure <= take_profit:
-        entry['TPR'] = True
-    else:
-        entry['TPR'] = False
+@killzone.route('/killzone', methods=['GET'])
+def determine_killzone():
+    # Parcourir tous les documents de la collection
+    for doc in collection.find():
+        # Récupérer l'heure d'ouverture de chaque document
+        opening_time = doc['dateAndTimeOpening'].time()
 
+        # Déterminer si l'heure d'ouverture se trouve dans l'une des plages horaires spécifiées
+        if (time(3, 0) <= opening_time <= time(6, 0)) or (time(9, 0) <= opening_time <= time(12, 0)) or (time(14, 0) <= opening_time <= time(17, 0)):
+            killzone = True
+        else:
+            killzone = False
+            
     return entry
+
