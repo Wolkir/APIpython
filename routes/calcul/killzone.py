4510a212c433e6b0ffa83a2e1746bfbb82c1dbcd
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 13:34:10 +0200

    Update killzone.py

--- routes/calcul/killzone.py
+++ routes/calcul/killzone.py
@@ -1,25 +1,25 @@
 from flask import Blueprint, jsonify, request
-from datetime import time
+from datetime import datetime, time
 
-#killzone_blueprint = Blueprint('killzone', __name__)
 killzone = Blueprint('killzone', __name__)
 
-#@killzone_blueprint.route('/killzone', methods=['POST'])
 def calculate_killzone(data):
-  
+    # Convertir la chaîne 'dateAndTimeOpening' en objet datetime
+    opening_datetime = datetime.strptime(data['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
+
+    # Récupérer l'heure d'ouverture de chaque document
+    opening_time = opening_datetime.time()
+
+    # Déterminer si l'heure d'ouverture se trouve dans l'une des plages horaires spécifiées
+    if (time(3, 0) <= opening_time <= time(6, 0)) or (time(9, 0) <= opening_time <= time(12, 0)) or (time(14, 0) <= opening_time <= time(17, 0)):
+        killzone_status = True
+    else:
+        killzone_status = False
+
+    return killzone_status
 
 
-    # Parcourir tous les documents de la collection
-    for doc in data:
-        # Récupérer l'heure d'ouverture de chaque document
-          opening_time = data['dateAndTimeOpening'].time()
 
-        # Déterminer si l'heure d'ouverture se trouve dans l'une des plages horaires spécifiées
-        if (time(3, 0) <= opening_time <= time(6, 0)) or (time(9, 0) <= opening_time <= time(12, 0)) or (time(14, 0) <= opening_time <= time(17, 0)):
-            killzone_status = True
-        else:
-            killzone_status = False
-       
-        return killzone_status
+
 
 
