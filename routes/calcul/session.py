d9e2a22f97238423cf421015a85c128c6d4382e5
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 14:31:47 +0200

    Update session.py

--- routes/calcul/session.py
+++ routes/calcul/session.py
@@ -1,25 +1,20 @@
-from flask import Flask, Blueprint, jsonify
-from pymongo import MongoClient
+from flask import Flask, Blueprint, jsonify, request
 from datetime import datetime
 
 session = Blueprint('session', __name__)
 
-# Connexion à la base de données MongoDB
-#client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
-#db = client['test']
-#collection = db['things']
+@session.route('/session', methods=['POST'])
+def determine_session():
+    data = request.json  # Récupérer les données JSON de la requête
 
-#@session.route('/session', methods=['GET'])
-def determine_session(data):
-    #data = request.json  # Récupérer les données JSON de la requête
-
-    sessions = []
     # Parcourir tous les documents pour déterminer la session en fonction de l'heure d'ouverture
     for doc in data:
-        opening_time = doc['dateAndTimeOpening']
+        # Vous pouvez ajuster le format d'heure ici en fonction de celui dans votre requête JSON
+        # Par exemple, si le format est "%Y-%m-%dT%H:%M:%S.%f%z", utilisez :
+        # opening_time = datetime.strptime(doc['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
+        opening_time = datetime.strptime(doc['dateAndTimeOpening'], "%Y-%m-%d %H:%M:%S.%f")
 
         # Déterminer la session en fonction de l'heure d'ouverture
-        session = ""
         if opening_time.hour >= 0 and opening_time.hour < 7:
             session = "AS"
         elif opening_time.hour >= 8 and opening_time.hour < 12:
@@ -29,6 +24,8 @@ def determine_session(data):
         else:
             session = "ND"
 
-      
+        # Ajouter la session au document actuel
+        doc['session'] = session
 
-    return data['session']
+    return jsonify(data)  # Retourner les données JSON mises à jour avec la clé 'session'
+
