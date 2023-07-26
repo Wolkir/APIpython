32ae21b6c6d1de276d242dc96fb7d6578452d381
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 14:49:20 +0200

    Update session.py

--- routes/calcul/session.py
+++ routes/calcul/session.py
@@ -1,29 +1,20 @@
-from flask import Blueprint, jsonify, request
-from datetime import datetime
-
-session = Blueprint('session', __name__)
+from datetime import datetime, time
 
 def determine_session(data):
-    # Parcourir tous les documents pour déterminer la session en fonction de l'heure d'ouverture
+    sessions = []
     for doc in data:
-        # Vous pouvez ajuster le format d'heure ici en fonction de celui dans votre requête JSON
-        # Par exemple, si le format est "%Y-%m-%dT%H:%M:%S.%f%z", utilisez :
-        # opening_time = datetime.strptime(doc['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
         opening_time = datetime.strptime(doc['dateAndTimeOpening'], "%Y-%m-%d %H:%M:%S.%f")
-
-        # Déterminer la session en fonction de l'heure d'ouverture
-        if opening_time.hour >= 0 and opening_time.hour < 7:
+        if time(0, 0) <= opening_time.time() < time(7, 0):
             session_value = "AS"
-        elif opening_time.hour >= 8 and opening_time.hour < 12:
+        elif time(8, 0) <= opening_time.time() < time(12, 0):
             session_value = "LD"
-        elif opening_time.hour >= 13 and opening_time.hour < 15:
+        elif time(13, 0) <= opening_time.time() < time(15, 0):
             session_value = "NY"
         else:
             session_value = "ND"
 
-        # Mettre à jour ou ajouter la clé 'session' dans le dictionnaire data
-        doc['session'] = session_value
+        sessions.append(session_value)
 
-    return session_value
+    return sessions
 
 
