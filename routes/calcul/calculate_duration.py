c0ba43e9d8a5d9ed0927f098c6ce809486aad4f6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 00:13:10 +0200

    Update calculate_duration.py

--- routes/calcul/calculate_duration.py
+++ routes/calcul/calculate_duration.py
@@ -5,14 +5,14 @@ calculate_duration = Blueprint('calculate_duration', __name__)
 
 def calculate_time_duration(data):
     try:
-        # Calculate the duration for each data entry in the list
-        for entry in data:
-            opening_time = datetime.strptime(entry['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
-            closure_time = datetime.strptime(entry['dateAndTimeClosure'], "%Y-%m-%dT%H:%M:%S.%f%z")
-            duration = closure_time - opening_time
-            entry['duration'] = duration  # Durée ajoutée directement sans conversion en chaîne de caractères
-        
-        return data  # Return the entire 'data' list with the 'duration' added to each element
+        # Calculate the duration between opening_time and closure_time
+        opening_time = datetime.strptime(data['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
+        closure_time = datetime.strptime(data['dateAndTimeClosure'], "%Y-%m-%dT%H:%M:%S.%f%z")
+        duration = closure_time - opening_time
+
+        # Add the duration to the data dictionary
+        data['duration'] = str(duration)
+
+        return data  # Return the data dictionary with the 'duration' added
     except Exception as e:
         return {"error": str(e)}  # Return an error response if an exception occurs
-
