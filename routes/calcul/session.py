7a376206d609be6ef2404aca6401efb905d0d48d
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-26 15:31:55 +0200

    Update session.py

--- routes/calcul/session.py
+++ routes/calcul/session.py
@@ -6,20 +6,16 @@ session = Blueprint('session', __name__)
 
 
 def determine_session(data):
+    opening_time = datetime.strptime(data['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
+    if time(0, 0) <= opening_time.time() < time(7, 0):
+        session_value = "AS"
+    elif time(8, 0) <= opening_time.time() < time(12, 0):
+        session_value = "LD"
+    elif time(13, 0) <= opening_time.time() < time(15, 0):
+        session_value = "NY"
+    else:
+        session_value = "ND"
 
-      
-        opening_time = datetime.strptime(data['dateAndTimeOpening'], "%Y-%m-%dT%H:%M:%S.%f%z")
-        if time(0, 0) <= opening_time.time() < time(7, 0):
-            session_value = "AS"
-        elif time(8, 0) <= opening_time.time() < time(12, 0):
-            session_value = "LD"
-        elif time(13, 0) <= opening_time.time() < time(15, 0):
-            session_value = "NY"
-        else:
-            session_value = "ND"
-
-        sessions.append(session_value)
-
-    return sessions
+    return session_value
 
 
