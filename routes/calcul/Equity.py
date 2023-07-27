71d6ef388c8c1afe88b1f6fa5fa26260e6b2a1a7
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 16:14:47 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, Blueprint
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
@@ -9,10 +9,15 @@ Equity = Blueprint('Equity', __name__)
 
 @Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
+    # Récupérer la dernière valeur de la clé "Equity" triée par ordre décroissant
+    latest_entry = db.test2_close.find_one({}, sort=[("Equity", DESCENDING)])
     
-    previous_equity = 2
+    # Mettre à jour previous_equity avec la dernière valeur de "Equity" si elle existe, sinon garder la valeur par défaut de 2
+    previous_equity = latest_entry.get("Equity", 2)
+    
     profit = data['profit']
-    equity = profit + 2
+    equity = profit + previous_equity
+    
     
     return equity
 
