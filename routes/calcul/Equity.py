e09fa3776feef97bcfe2ea1f4d14f9bde271617f
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 14:51:35 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, Blueprint
 from pymongo import MongoClient
+from functools import reduce
 
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
@@ -12,12 +13,17 @@ def calculate_equity(data):
     # Get the last equity value from "test2_close" collection
     last_equity = db["test2_close"].find_one({}, sort=[('_id', -1)])['Equity']
 
-    # Calculate new equity for each document in 'data'
-    for document in data:
+    # Define a function to calculate the cumulative equity
+    def calculate_cumulative_equity(previous_equity, document):
         if 'profit' in document:
             profit = document['profit']
-            last_equity += profit
-            document['Equity'] = last_equity
+            current_equity = previous_equity + profit
+            document['Equity'] = current_equity
+            return current_equity
+        return previous_equity
+
+    # Use reduce to calculate the cumulative equity for each document in 'data'
+    final_equity = reduce(calculate_cumulative_equity, data, last_equity)
 
     return data
 
