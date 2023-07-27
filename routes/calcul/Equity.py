a61171404801845128708187e2af3532b5595fc2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 23:16:10 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -5,20 +5,19 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
 Equity = Blueprint('Equity', __name__)
+
 @Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
-    latest_entry = db.test2_close.find({}, sort=[("Equity", DESCENDING)], limit=1)
+    latest_entry = db.test2_close.find_one({}, sort=[("Equity", DESCENDING)])
 
-    # Vérifier si latest_entry est non vide
-    if latest_entry.count() > 0:
-        # Récupérer la première entrée (la dernière valeur) et extraire la clé "Equity"
-        previous_entry = latest_entry[0].get("Equity", 2)
+    # Vérifier si latest_entry existe et si "Equity" est une valeur numérique (int ou float)
+    if latest_entry and isinstance(latest_entry["Equity"], (float, int)):
+        previous_entry = float(latest_entry["Equity"])
     else:
-        previous_entry = 2  # Valeur par défaut si la collection est vide
+        previous_entry = 2.0  # Valeur par défaut si "Equity" n'est pas un nombre ou latest_entry est None
 
     profit = data['profit']
 
     equity = profit + previous_entry
 
     return str(equity)
-
