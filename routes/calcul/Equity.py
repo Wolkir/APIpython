ca8b05f730016aa4362f7a3b5979aae39d90bbca
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 14:24:57 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -7,36 +7,15 @@ db = client["test"]
 
 Equity = Blueprint('Equity', __name__)
 
-def calculate_equity(username, data):
-    try:
-        # Récupérer la dernière valeur d'équité de la collection username_close
-        collection_name = f"{username}_close"
-        collection = db[collection_name]
-        last_entry = collection.find_one(sort=[('_id', -1)])
-        previous_equity = last_entry.get('equity', 0.0)
+@Equity.route('/equity', methods=['GET'])
+def calculate_equity(data):
+    previous_equity = 0
 
-        # Récupérer la valeur de profit depuis l'objet data
-        profit = data.get('profit', 0.0)
+    for document in data:
+        if 'profit' in document:
+            profit = document['profit']
+            equity = previous_equity + profit
+            document['equity'] = equity
+            previous_equity = equity
 
-        # Calculer la nouvelle valeur d'équité
-        equity = previous_equity + profit
-
-        # Ajouter la nouvelle valeur d'équité à la variable Equity dans l'objet data
-        data['Equity'] = equity
-
-        return data  # Renvoyer l'objet data avec la nouvelle équité ajoutée
-    except Exception as e:
-        return {"error": str(e)}  # Renvoyer une réponse d'erreur en cas d'exception
-
-@app.route('/calculate_equity', methods=['POST'])
-def calculate_equity_route(data):
-    try:
-        data = request.json
-        username = data.get('username')
-        result = calculate_equity(username, data)
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return data
