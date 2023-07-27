a173bb946feadf5cdf5f58ab660cfc4fd64377f1
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 12:55:40 +0100

    Update createStrategie.py

--- connexion/strategie/createStrategie.py
+++ connexion/strategie/createStrategie.py
@@ -3,28 +3,26 @@ from flask_pymongo import PyMongo
 
 createStrategie = Blueprint('createStrategie', __name__)
 
-def setup_createStrategie_routes(app):
-    app.config['MONGO_URI'] = 'mongodb://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-    mongo = PyMongo(app)
+@createStrategie.route('/createStrategie', methods=['POST'])
+def create_strategie():
+    data = request.json
+    username = data.get('username')
+    nomStrategie = data.get('nomStrategie')
 
-    @createStrategie.route('/createStrategie', methods=['POST'])
-    def create_strategie():
-        data = request.json
-        username = data.get('username')
-        nomStrategie = data.get('nomStrategie')
+    try:
+        mongo = PyMongo(current_app)
+        collection = mongo.db['strategies']
+        strategies = list(collection.find({"nomStrategie": nomStrategie}))
+        if strategies:
+            return jsonify({"error": "Cette stratégie existe déjà"}), 400
 
-        try:
-            existing_strategie = mongo.db.strategies.find_one({"nomStrategie": nomStrategie})
-            if existing_strategie:
-                return jsonify({"error": "Cette stratégie existe déjà"}), 400
+        strategie = {
+            "nomStrategie": nomStrategie,
+            "username": username
+        }
+        mongo.db.strategies.insert_one(strategie)
 
-            strategie = {
-                "nomStrategie": nomStrategie,
-                "username": username
-            }
-            mongo.db.strategies.insert_one(strategie)
-
-            return jsonify({"message": "Stratégie enregistrée avec succès"}), 200
-        except Exception as e:
-            current_app.logger.error(f"Error occurred: {e}")
-            return jsonify({"error": "Erreur lors de la enregistrement des stratégies pour l'utilisateur donné", "details": str(e)}), 500
+        return jsonify({"message": "Stratégie enregistrée avec succès"}), 200
+    except Exception as e:
+        current_app.logger.error(f"Error occurred: {e}")
+        return jsonify({"error": "Erreur lors de la enregistrement des stratégies pour l'utilisateur donné", "details": str(e)}), 500
