5d51f03c9d2cf0165538fee5d5bf11716cb29355
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 15:45:34 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, Blueprint
 from pymongo import MongoClient
 
-
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
@@ -9,7 +8,7 @@ db = client["test"]
 Equity = Blueprint('Equity', __name__)
 
 @Equity.route('/equity', methods=['POST'])
-def calculate_equity(data):
+def calculate_equity():
     data = request.get_json()
 
     if 'profit' in data and isinstance(data['profit'], (int, float)):
@@ -19,3 +18,10 @@ def calculate_equity(data):
         return str(equity)  # Retourne la valeur de l'équité sous forme de chaîne de caractères
     else:
         return 'Invalid data. Expected JSON with "profit" field as a number.', 400
+
+# Enregistrez le Blueprint sur l'application principale
+app.register_blueprint(Equity)
+
+# Exécutez le serveur Flask
+if __name__ == '__main__':
+    app.run()
