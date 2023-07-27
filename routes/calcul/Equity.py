47a4fce14045bccddb6f3b46494a6bbaf0a226b2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 23:32:10 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -8,13 +8,18 @@ Equity = Blueprint('Equity', __name__)
 
 @Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
-    latest_entry = db.things_close.find_one({}, sort=[("Equity", DESCENDING)])
+    latest_entry_cursor = db.test2_close.find({}, sort=[("_id", DESCENDING)], limit=1)
 
-    # Vérifier si latest_entry existe et si "Equity" est une valeur numérique (int ou float)
-    if latest_entry and isinstance(latest_entry["Equity"], (float, int)):
-        previous_entry = float(latest_entry["Equity"])
+    # Vérifier si latest_entry_cursor contient des documents
+    if latest_entry_cursor.count() > 0:
+        latest_entry = latest_entry_cursor[0]
+        # Vérifier si "Equity" existe dans latest_entry et est une valeur numérique (int ou float)
+        if "Equity" in latest_entry and isinstance(latest_entry["Equity"], (float, int)):
+            previous_entry = float(latest_entry["Equity"])
+        else:
+            previous_entry = 2.0  # Valeur par défaut si "Equity" n'est pas un nombre
     else:
-        previous_entry = 2.0  # Valeur par défaut si "Equity" n'est pas un nombre ou latest_entry est None
+        previous_entry = 2.0  # Valeur par défaut si la collection est vide
 
     profit = data['profit']
 
