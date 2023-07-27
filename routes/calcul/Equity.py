3931bc528ceb1b7870b0a08835833d587fb2f342
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 15:00:24 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -1,21 +1,30 @@
-from flask import Flask, request, jsonify, Blueprint
+from flask import Blueprint
 from pymongo import MongoClient
-from functools import reduce
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
 Equity = Blueprint('Equity', __name__)
 
 @Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
+    # Initialize a variable to store the previous equity value
     previous_equity = 0
 
+    # Iterate over each data entry
     for document in data:
         if 'profit' in document:
             profit = document['profit']
-            equity = previous_equity + profit
-            document['equity'] = equity
-            previous_equity = equity
 
-    return data
+            # Calculate the new equity value by adding the previous equity and the current profit
+            equity = previous_equity + profit
+
+            # Update the data entry with the new equity value
+            document['equity'] = equity
+
+            previous_equity = equity  # Update the previous equity value for the next iteration
+
+    # Get the last data entry from the list
+    last_entry = data[-1]
+
+    # Get the equity value from the last data entry
+    last_equity = last_entry['equity']
+
+    return last_equity  # Return the last calculated equity value
