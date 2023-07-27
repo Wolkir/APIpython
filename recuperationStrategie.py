2427541a60bff3172595a50bc610c897628f08a4
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 09:54:32 +0100

    Update recuperationStrategie.py

--- recuperationStrategie.py
+++ recuperationStrategie.py
@@ -3,22 +3,26 @@ from flask_pymongo import PyMongo
 
 recuperationStrategie = Blueprint('recuperationStrategie', __name__)
 
+@recuperationStrategie.route('/recuperationStrategie', methods=['GET'])
+def get_recuperationStrategie():
+    try:
+        username = request.args.get('username')
+
+        if not username:
+            return jsonify({"error": "L'argument 'username' est manquant dans la requête"}), 400
+
+        app.config['MONGO_URI'] = 'mongodb://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
+        mongo = PyMongo(app)
+        collection = mongo.db['strategies']
+
+        strategies = list(collection.find({"username": username}))
+
+        return jsonify(strategies), 200
+
+    except Exception as e:
+        return jsonify({"error": "Erreur lors de la récupération des stratégies pour l'utilisateur donné", "details": str(e)}), 500
+
 def setup_recuperationStrategie(app):
-    @recuperationStrategie.route('/recuperationStrategie', methods=['GET'])
-    def get_recuperationStrategie():
-        try:
-            username = request.args.get('username')
-
-            if not username:
-                return jsonify({"error": "L'argument 'username' est manquant dans la requête"}), 400
-
-            app.config['MONGO_URI'] = 'mongodb://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-            mongo = PyMongo(app)
-            collection = mongo.db['strategies']
-
-            strategies = list(collection.find({"username": username}))
-
-            return jsonify(strategies), 200
-
-        except Exception as e:
-            return jsonify({"error": "Erreur lors de la récupération des stratégies pour l'utilisateur donné", "details": str(e)}), 500
+    # Vous pouvez effectuer d'autres opérations si nécessaire
+    # avant de retourner le blueprint
+    return recuperationStrategie
