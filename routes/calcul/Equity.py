54004718c395d1e6f68c4ff754a03a80e7a2918d
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 12:50:29 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -5,14 +5,12 @@ from flask import Blueprint
 
 Equity = Blueprint('Equity', __name__)
 
-@Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
     # Initialize a variable to store the previous equity value
     previous_equity = 0.0  # Utiliser 0.0 pour indiquer que c'est un nombre à virgule flottante
 
-    # Iterate over each data entry
-    for document in data:
-        if 'profit' in document:
+    try:
+        for document in data:
             profit = document['profit']
 
             # Calculate the new equity value by adding the previous equity and the current profit
@@ -23,10 +21,12 @@ def calculate_equity(data):
 
             previous_equity = equity  # Update the previous equity value for the next iteration
 
-    # Get the last data entry from the list
-    last_entry = data[-1]
+        # Get the last data entry from the list
+        last_entry = data[-1]
 
-    # Get the equity value from the last data entry
-    last_equity = last_entry['equity']
+        # Get the equity value from the last data entry
+        last_equity = last_entry['equity']
 
-    return last_equity  # Renvoyer la nouvelle équité sous forme de nombre à virgule flottante
+        return last_equity  # Renvoyer la nouvelle équité sous forme de nombre à virgule flottante
+    except Exception as e:
+        return {"error": str(e)}  # Return an error response if an exception occurs
