037254f76b0308a02be261f2a99dbc3bda7710d3
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 22:50:18 +0200

    Update Equity.py

--- routes/calcul/Equity.py
+++ routes/calcul/Equity.py
@@ -10,10 +10,17 @@ Equity = Blueprint('Equity', __name__)
 @Equity.route('/equity', methods=['GET'])
 def calculate_equity(data):
     latest_entry = db.test2_close.find_one({}, sort=[("Equity", DESCENDING)])
-    previous_entry = latest_entry.get("Equity", 2)
+
+    # Vérifier si latest_entry existe et si "Equity" est une valeur numérique (int ou float)
+    if latest_entry and isinstance(latest_entry["Equity"], (float, int)):
+        previous_entry = float(latest_entry["Equity"])
+    else:
+        previous_entry = 2.0  # Valeur par défaut si "Equity" n'est pas un nombre ou latest_entry est None
+
     profit = data['profit']
+
     equity = profit + previous_entry
-    
+
     return str(equity)
 
 
