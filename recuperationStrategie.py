3a6fb0f854e5531d875a04d01725001eb9e32ad1
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 12:24:29 +0100

    Update recuperationStrategie.py

--- recuperationStrategie.py
+++ recuperationStrategie.py
@@ -1,26 +1,19 @@
 from flask import Blueprint, jsonify, request, current_app
 from flask_pymongo import PyMongo
 
-# Créez le blueprint
 recuperationStrategie = Blueprint('recuperationStrategie', __name__)
 
-# Définissez la fonction pour récupérer les stratégies
 @recuperationStrategie.route('/recuperationStrategie', methods=['GET'])
-def recuperationStrategieGet():
+def get_recuperationStrategie():
     try:
-        # Récupérer l'argument "username" de la requête
         username = request.args.get('username')
 
         if not username:
             return jsonify({"error": "L'argument 'username' est manquant dans la requête"}), 400
 
-        # Connexion à la base de données MongoDB
-        app = recuperationStrategie.app
-        app.config['MONGO_URI'] = 'mongodb://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-        mongo = PyMongo(app)
+        mongo = PyMongo(current_app)
         collection = mongo.db['strategies']
 
-        # Récupérer les données de la base de données pour l'utilisateur donné
         strategies = list(collection.find({"username": username}))
 
         return jsonify(strategies), 200
