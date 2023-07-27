062effb652c6ce762489e39d06e5f1c35e04655c
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-27 13:08:06 +0100

    Update suppressionStrategie.py

--- connexion/strategie/suppressionStrategie.py
+++ connexion/strategie/suppressionStrategie.py
@@ -1,17 +1,16 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
 from flask_pymongo import PyMongo
 
 suppressionStrategie = Blueprint('suppressionStrategie', __name__)
 
 @suppressionStrategie.route('/suppressionStrategie', methods=['POST'])
-def suppression_strategie(app):
+def suppression_strategie():
     data = request.json
     username = data.get('username')
     nomStrategie = data.get('nomStrategie')
 
     try:
-        app.config['MONGO_URI'] = 'mongodb://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority'
-        mongo = PyMongo(app)
+        mongo = PyMongo(current_app)
         collection = mongo.db['strategies']
 
         result = collection.delete_many({"nomStrategie": nomStrategie})
@@ -22,4 +21,5 @@ def suppression_strategie(app):
             return jsonify({"error": "La stratégie n'a pas été trouvée"}), 404
 
     except Exception as e:
-        return jsonify({"error": "Erreur lors de la suppression de la stratégie"}), 500
+        current_app.logger.error(f"Error occurred: {e}")
+        return jsonify({"error": "Erreur lors de la enregistrement des stratégies pour l'utilisateur donné", "details": str(e)}), 500
