b4791f6528dfc0ce37c46b492037073ad42b5bb5
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-28 18:18:44 +0200

    Update averagegain.py

--- routes/calcul/average/averagegain.py
+++ routes/calcul/average/averagegain.py
@@ -35,6 +35,6 @@ def calculate_average_gain(data):
 
     # Insérer le winrate_value dans la collection "unitaire"
     unitaire_collection = db[collection_unitaire]
-    unitaire_collection.update_one({}, {'$set': {'averagegain': str(average_gain)}}, upsert=True)
+    unitaire_collection.update_one({}, {'$set': {'averagegain': (average_gain)}}, upsert=True)
  
 
