d4b4f14145296cae52d03663a6c06bbee5ddd7d2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-28 16:12:07 +0200

    Create average_rr.py

---
+++ routes/calcul/average/average_rr.py
@@ -0,0 +1,26 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+
+average_rr = Blueprint('average_rr', __name__)
+
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/test?retryWrites=true&w=majority')
+db = client['test']
+collection = db['things']
+
+@average_rr.route('/average_rr', methods=['GET'])
+def calculate_average_rr():
+    rr_values = []
+    for document in collection.find():
+        if "RR" in document:
+            rr_values.append(document["RR"])
+
+    print("Valeurs de RR dans la collection 'things':", rr_values)
+
+    rr_total = sum(rr_values)
+    rr_count = len(rr_values)
+    average_rr = rr_total / rr_count if rr_count > 0 else 0
+
+    unitaire_collection = db['unitaire']
+    unitaire_collection.update_one({}, {"$set": {"RRaverage": average_rr}}, upsert=True)
+
+    return jsonify({"average_rr": average_r
