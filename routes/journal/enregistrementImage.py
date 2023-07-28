2a4fd48bcdfee133be0d1058d955beecb61be0d5
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-28 16:34:33 +0100

    Create enregistrementImage.py

---
+++ routes/journal/enregistrementImage.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify, current_app
+import pymongo
+from bson import ObjectId
+import gridfs
+import io
+
+
+enregistrerImage = Blueprint('enregistrerImage', __name__)
+
+@enregistrerImage.route('/enregistrerImage', methods=['POST'])
+def enregistrer_image():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'message': 'Aucune image trouvée dans la requête'}), 400
+
+        image = request.files['image']
+
+        db = current_app.config['MONGO_DB']
+        fs = gridfs.GridFS(db)
+
+        image_id = fs.put(image.stream, filename=image.filename)
+
+        return jsonify({'message': 'Image enregistrée avec succès', 'image_id': str(image_id)})
+    except Exception as e:
+        return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
