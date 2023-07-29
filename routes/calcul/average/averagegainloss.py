d47e06d3ab8c214dcba50c11d3ad77a44dfefc8e
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-30 00:22:33 +0200

    Update averagegainloss.py

--- routes/calcul/average/averagegainloss.py
+++ routes/calcul/average/averagegainloss.py
@@ -24,12 +24,7 @@ def calculate_average_gain_loss_rr(data):
     negative_losses_count = 0
     negative_ticket_numbers = set()
 
-    # Calcul de RR
-    price_close = data.get('priceClosure')
-    price_opening = data.get('priceOpening')
-    stop_loss = data.get('stopLoss')
-    rr = (price_close - price_opening) / (price_opening - stop_loss)
-    rr = round(rr, 2)
+    rr_values = []  # Liste pour stocker les valeurs de RR
 
     # Parcourir les documents de la collection
     for doc in collection.find():
@@ -46,13 +41,35 @@ def calculate_average_gain_loss_rr(data):
             negative_losses_count += 1
             negative_ticket_numbers.add(ticket_number)
 
+        # Calcul de RR pour chaque document
+        price_close = doc.get('priceClosure')
+        price_opening = doc.get('priceOpening')
+        stop_loss = doc.get('stopLoss')
+        rr = (price_close - price_opening) / (price_opening - stop_loss)
+        rr = round(rr, 2)
+
+        rr_values.append(rr)  # Ajouter la valeur de RR à la liste
+
     # Calcul de la moyenne des gains et pertes
     average_gain = positive_gains_total / positive_gains_count if positive_gains_count > 0 else 0
     average_loss = negative_losses_total / negative_losses_count if negative_losses_count > 0 else 0
 
+    # Calcul de la moyenne des valeurs de RR
+    rr_total = sum(rr_values)
+    rr_count = len(rr_values)
+    average_rr = rr_total / rr_count if rr_count > 0 else 0
+
     # Insérer les valeurs dans la collection "unitaire"
     unitaire_collection = db[collection_unitaire]
-    unitaire_collection.update_one({}, {'$set': {'averagegain2': average_gain, 'averagelosse2': average_loss, 'RR2': rr}}, upsert=True)
-
-
+    unitaire_collection.update_one(
+        {},
+        {
+            '$set': {
+                'averagegain': average_gain,
+                'averagelosse': average_loss,
+                'RRaverage': average_rr
+            }
+        },
+        upsert=True
+    )
 
