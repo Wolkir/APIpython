d309e01828ae489ccb439c845928749efd8b0c5b
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-29 13:45:48 +0100

    Update enregistrementImage.py

--- routes/journal/enregistrementImage.py
+++ routes/journal/enregistrementImage.py
@@ -27,21 +27,3 @@ def enregistrer_image():
     except Exception as e:
         current_app.logger.error(f"Error occurred: {e}")
         return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
-
-@enregistrerImage.route('/enregistrerImage', methods=['GET'])
-def recuperation_image():
-    try:
-        image_id = request.args.get('imageId', None)
-        app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority'
-        mongo = MongoClient(app.config['MONGO_URI'])
-        db = mongo["test"]
-        fs = gridfs.GridFS(db)
-
-        image = fs.get(ObjectId(image_id))
-        if image is None:
-            return jsonify({'message': 'Image non trouvée'}), 500
-
-        return send_file(io.BytesIO(image.read()), mimetype='image/jpeg')
-    except Exception as e:
-        current_app.logger.error(f"Error occurred: {e}")
-        return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
