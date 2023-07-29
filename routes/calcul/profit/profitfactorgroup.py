b5d046832fa493aae7b58e934c7db7ecacbd8f4e
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-29 20:31:50 +0200

    Update profitfactorgroup.py

--- routes/calcul/profit/profitfactorgroup.py
+++ routes/calcul/profit/profitfactorgroup.py
@@ -8,11 +8,9 @@ client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm
 db = client['test']
 
 # Blueprint pour les routes profitfactorgroup
-profitfactorgroup= Blueprint('profitfactorgroup', __name__)
+profitfactorgroup = Blueprint('profitfactorgroup', __name__)
 
 @profitfactorgroup.route('/profitfactorgroup', methods=['GET'])
-
-# Fonction pour calculer le profit factor
 def calculate_profit_factor_custom(collection, filter_query):
     username = request.args.get('username')
     collection_name = f"{username}_close"
@@ -49,14 +47,16 @@ def calculate_profit_factor_group(data):
     else:
         filter_query = {}
 
+    # Pass both collection and filter_query to the function
     total_profit, total_loss, profit_factor = calculate_profit_factor_custom(collection, filter_query)
+
     unitaire_collection = db[collection_unitaire]
 
-    if transaction_type == 'buy':
+    if transaction_type == 'Buy':
         unitaire_collection.update_one({}, {'$set': {'profitfactor_long': profit_factor}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_loss_long': total_loss}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_gain_long': total_profit}}, upsert=True)
-    elif transaction_type == 'sell':
+    elif transaction_type == 'Sell':
         unitaire_collection.update_one({}, {'$set': {'profitfactor_short': profit_factor}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_loss_short': total_loss}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_gain_short': total_profit}}, upsert=True)
@@ -64,8 +64,4 @@ def calculate_profit_factor_group(data):
         unitaire_collection.update_one({}, {'$set': {'profitfactor_all': profit_factor}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_loss_all': total_loss}}, upsert=True)
         unitaire_collection.update_one({}, {'$set': {'total_gain_all': total_profit}}, upsert=True)
-
-   
-
-
 
