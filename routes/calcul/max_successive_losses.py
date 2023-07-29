8236646160ca47d9df2f7196bf67ed6398e0050c
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-29 12:34:30 +0200

    Update max_successive_losses.py

--- routes/calcul/max_successive_losses.py
+++ routes/calcul/max_successive_losses.py
@@ -19,6 +19,13 @@ def find_max_successive_losses(data):
     current_successive_losses_count = 0
     previous_identifier = None
 
+    # Initialisation des variables
+    max_successive_losses_count = 0
+    current_successive_losses_count = 0
+    previous_identifier = None
+    successive_losses_counts = []  # Initialisation de la liste
+
+
     # Parcourir les documents de la collection
     for doc in collection.find().sort("identifier"):
         profit = doc['profit']
@@ -30,12 +37,12 @@ def find_max_successive_losses(data):
             else:
                 current_successive_losses_count += 1
 
-            if current_successive_losses_count > max_successive_losses_count:
-                max_successive_losses_count = current_successive_losses_count
+            successive_losses_counts.append(current_successive_losses_count)
 
+        previous_identifier = identifier
 
+    max_successive_losses_count = max(successive_losses_counts)
 
-
-    # Insérer le winrate_value dans la collection "unitaire"
+    # Insérer le max_successive_loss dans la collection "unitaire"
     unitaire_collection = db[collection_unitaire]
-    unitaire_collection.update_one({}, {'$set': {'max_successive_loss': (max_successive_losses_count)}}, upsert=True)
+    unitaire_collection.update_one({}, {'$set': {'max_successive_loss': max_successive_losses_count}}, upsert=True)
