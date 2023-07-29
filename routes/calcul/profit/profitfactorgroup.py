9a412a9d5f56a3c11113a3ee6fe15d98e53c07ee
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-29 22:51:15 +0200

    Update profitfactorgroup.py

--- routes/calcul/profit/profitfactorgroup.py
+++ routes/calcul/profit/profitfactorgroup.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, jsonify, request
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,15 +7,22 @@ app = Flask(__name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-# Blueprint pour les routes profitfactorgroup
-profitfactorgroup = Blueprint('profitfactorgroup', __name__)
+@app.route('/profitfactorgroup', methods=['GET'])
+def calculate_profit_factor_group():
+    username = request.args.get('username')
+    transaction_type = request.args.get('type', None)
 
-# Fonction pour calculer le profit factor
-def calculate_profit_factor_custom(collection, filter_query):
+    collection_name = f"{username}_close"
+    collection_unitaire = f"{username}_unitaire"
+    collection = db[collection_name]
+
+    # Calcul du profit total et de la perte totale pour les transactions du type spécifié
     total_profit = 0
     total_loss = 0
 
     # Parcourir les documents de la collection
+    filter_query = {} if transaction_type is None else {"typeOfTransaction": transaction_type}
+
     for doc in collection.find(filter_query):
         profit = doc['profit']
         if profit > 0:
@@ -26,43 +33,13 @@ def calculate_profit_factor_custom(collection, filter_query):
     # Calcul du profit factor
     profit_factor = total_profit / abs(total_loss)
 
-    return total_profit, total_loss, profit_factor
-
-@profitfactorgroup.route('/profitfactorgroup', methods=['GET'])
-def calculate_profit_factor_group():
-    username = request.args.get('username')
-    collection_name = f"{username}_close"
-    collection_unitaire = f"{username}_unitaire"
-    collection = db[collection_name]
-    transaction_type = request.args.get('type')  # Paramètre pour spécifier le type de transaction (buy, sell ou all)
-
-    if transaction_type == 'Buy':
-        filter_query = {"typeOfTransaction": "Buy"}
-    elif transaction_type == 'Sell':
-        filter_query = {"typeOfTransaction": "Sell"}
-    else:
-        filter_query = {}
-
-    # Pass both collection and filter_query to the function
-    total_profit, total_loss, profit_factor = calculate_profit_factor_custom(collection, filter_query)
-
+    # Insérer le profit factor dans la collection "unitaire"
     unitaire_collection = db[collection_unitaire]
-
-    if transaction_type == 'Buy':
-        unitaire_collection.update_one({}, {'$set': {'profitfactor_long': profit_factor}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_loss_long': total_loss}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_gain_long': total_profit}}, upsert=True)
-    elif transaction_type == 'Sell':
-        unitaire_collection.update_one({}, {'$set': {'profitfactor_short': profit_factor}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_loss_short': total_loss}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_gain_short': total_profit}}, upsert=True)
-    else:
-        unitaire_collection.update_one({}, {'$set': {'profitfactor_all': profit_factor}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_loss_all': total_loss}}, upsert=True)
-        unitaire_collection.update_one({}, {'$set': {'total_gain_all': total_profit}}, upsert=True)
-
-# Register the blueprint
-app.register_blueprint(profitfactorgroup)
-
-if __name__ == '__main__':
-    app.run()
+    if transaction_type is None:
+        unitaire_collection.update_one({}, {'$set': {'profitfactor': profit_factor}}, upsert=True)
+        unitaire_collection.update_one({}, {'$set': {'total loss': total_loss}}, upsert=True)
+        unitaire_collection.update_one({}, {'$set': {'total gain': total_profit}}, upsert=True)
+    elif transaction_type == "Buy":
+        unitaire_collection.update_one({}, {'$set': {'profitfactorlong': profit_factor}}, upsert=True)
+        unitaire_collection.update_one({}, {'$set': {'total loss long': total_loss}}, upsert=True)
+        unitaire_collection.update_one({}, {'$set': {'total gain long': total_profit}}, upsert=True)
