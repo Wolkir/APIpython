b6c13cf86555ac75f5f3c7ebfc408ce6762d7bba
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-31 00:19:06 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -21,6 +21,7 @@ from routes.calcul.profit.profitfactorgroup import calculate_profit_factor_group
 from routes.calcul.winrategroup import calculate_winrate_group
 from routes.calcul.average.averagegainloss import calculate_average_gain_loss_rr
 from routes.calcul.winrrtflat import calculate_winrrtflat
+from routes.calcul.sharp import calculate_sharp_ratio
 
 #from routes.calcul.profit.profitfactor  import calculate_profit_factor // remplacé par le code groupé profit_factor_group
 #from routes.calcul.profit.profitfactorlong  import calculate_profit_factor_long  // remplacé par le code groupé profit_factor_group
@@ -203,6 +204,7 @@ def save_trade_request():
         calculate_winrate_group(data)
         calculate_average_gain_loss_rr(data) 
         calculate_winrrtflat(data)
+        calculate_sharp_ratio(data)
              
         #calculate_profit_factor(data)  // remplacé par le code groupé profit_factor_group        
         #calculate_profit_factor_long(data)   // remplacé par le code groupé profit_factor_group            
