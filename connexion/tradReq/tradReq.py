730a82c41651db0d8f1807c89293a3ffbddbdf56
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-31 14:25:52 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -73,6 +73,7 @@ def save_trade_request():
            # Mettre à jour UNIQUEMENT la variable stopLoss dans la collection des ordres "Open"
            open_orders = db[f"{username}_open"]
            open_orders.update_one({"identifier": data.get('identifier')}, {"$set": {"stopLoss": data.get('stopLoss')}})
+           calculate_rrt(data)
            open_orders.update_one({"identifier": data.get('identifier')}, {"$set": {"RRT": data.get('RRT')}})
             
         
