d7b5051eb2f4d06564758e71a956fca5f693ff96
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 19:14:51 +0200

    Update BE.py

--- routes/calcul/BE.py
+++ routes/calcul/BE.py
@@ -8,22 +8,16 @@ BE = Blueprint('BE', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-def rr_calculation(data):
-  
+def find_BE(data):
     price_close = data['priceClosure']
     price_opening = data['priceOpening']
     stop_loss = data['stopLoss']
     rrcalculation = (price_close - price_opening) / (price_opening - stop_loss)
     rrcalculation = round(rrcalculation, 2)
-    return rrcalculation  # Renvoie la valeur de la clé "RR"
 
+    resultBE = {}  # Initialize the 'resultBE' dictionary
 
-def find_BE(data):
-    resultBE = {}  # Initialize the 'order' dictionary
-
-    rrcalculation = rr_calculation(data)
-
-    if data['closurePosition'] == "Close" and rrcalculation is not None and -0.5 < rrcalculation < 0.5:
+    if data['closurePosition'] == "Close" and -0.5 < rrcalculation < 0.5:
         resultBE['BE'] = True
     else:
         resultBE['BE'] = False
