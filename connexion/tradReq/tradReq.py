eeb573cfa895a1a4e2dfd706444e3255c9678e02
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 22:54:06 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -11,8 +11,6 @@ from routes.calcul.RR import calculate_rr
 from routes.calcul.RRT import calculate_rrt
 from routes.calcul.Equity import calculate_equity
 from routes.calcul.weekday import add_weekday
-#from routes.calcul.BE import find_BE
-
 
 
 #code groupé
@@ -53,7 +51,7 @@ trade_blueprint = Blueprint('trade', __name__)
 SLOpen = {}
 RROpen = {}
 TPOpen = {}
-
+daily_trade_counts = {}
 def compare_passwords(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
 
@@ -158,10 +156,9 @@ def save_trade_request():
             weekday_str = add_weekday(data)
             data['Day'] = weekday_str
             tradecount=calculate_tradercount(data)
+            data['tradercount'] = tradecount
+        
 
-           
-            #resultBE = find_BE(data)
-            #data['BE'] = resultBE
        
                        
 
@@ -186,8 +183,8 @@ def save_trade_request():
             data['Day'] = weekday_str
             
             RROpen[data.get('identifier')] = rrt
-            #tradecount=calculate_tradercount(data)
-
+            tradecount=calculate_tradercount(data)
+            data['tradercount'] = tradecount
           
             
         # Insert the data into the collection
@@ -216,7 +213,7 @@ def save_trade_request():
             "profit": data.get('profit'),
             "commission": data.get('commision'),
             "closurePosition": data.get('closurePosition'),
-           # "tradercount" : daily_trade_counts.get(data.get('identifier')),
+            "tradercount" : (data.get('tradecount')),
             "balance": data.get('balance'),
             "broker": data.get('broker'),
             "annonceEconomique": None,
@@ -234,7 +231,6 @@ def save_trade_request():
             "RR": data.get('RR'),
             "RROpen": RROpen.get(data.get('identifier')),
             "RRT": data.get('RRT'),
-           # "BE": data.get('BE'),
             "Equity": data.get('Equity'),
             "Day": data.get('Day'),           
             "strategie": None,
