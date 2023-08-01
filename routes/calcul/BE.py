015cca06b4f0269ecee0f20e67f6b02aad99bc31
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 16:54:37 +0200

    Update BE.py

--- routes/calcul/BE.py
+++ routes/calcul/BE.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, jsonify, request
+from flask import Flask, Blueprint, jsonify
 from pymongo import MongoClient
 
 BE = Blueprint('BE', __name__)
@@ -6,25 +6,16 @@ BE = Blueprint('BE', __name__)
 # Connexion à la base de données MongoDB
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
-data = request.json
 
-def find_BE(username):
-    username = data.get('username')
-    collection_name = f"{username}_close"
-    collection = db[collection_name]
 
-    resultBE = []
-    for order in collection.find({'type': 'Close'}):
-        rr = order.get('RR', None)
-        if rr is not None and -0.5 < rr < 0.5:
-            order['BE'] = True
-        else:
-            order['BE'] = False
-        resultBE.append(order)
+def find_BE(data):
+   resultBE = {}  # Initialize the 'order' dictionary
 
-    return resultBE
+    rr = data['rr']
 
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.register_blueprint(BE)
-    app.run()
+    if data['closurePosition'] == "Close" and rr is not None and -0.5 < rr < 0.5:
+        resultBE['BE'] = True
+    else:
+        resultBE['BE'] = False
+
+    return resultBE  # Renvoie la valeur de BE
