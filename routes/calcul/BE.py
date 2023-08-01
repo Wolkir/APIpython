561c980f30a5483b4beb414cb5b2929edcc0c8e3
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 15:59:14 +0200

    Update BE.py

--- routes/calcul/BE.py
+++ routes/calcul/BE.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, request, jsonify
+from flask import Flask, Blueprint, jsonify
 from pymongo import MongoClient
 
 BE = Blueprint('BE', __name__)
@@ -7,29 +7,20 @@ BE = Blueprint('BE', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-
-@BE.route('/BE', methods=['GET'])
 def find_BE(data):
-    try:
-        data = request.json
-        username = data.get('username')
-        collection_name = f"{username}_close"
-        collection = db[collection_name]
+    collection_name = f"{username}_close"
+    collection = db[collection_name]
 
-        resultBE = []
-        for order in collection.find({'type': 'Close'}).limit(100):  # Limiting to the first 100 results for testing
-            rr = order.get('RR', None)
-            if rr is not None and -0.5 < rr < 0.5:
-                order['BE'] = True
-            else:
-                order['BE'] = False
-            resultBE.append(order)
+    resultBE = []
+    for order in collection.find({'type': 'Close'}):
+        rr = order.get('RR', None)
+        if rr is not None and -0.5 < rr < 0.5:
+            order['BE'] = True
+        else:
+            order['BE'] = False
+        resultBE.append(order)
 
-        return jsonify(resultBE)
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    return resultBE
 
 if __name__ == '__main__':
     app = Flask(__name__)
