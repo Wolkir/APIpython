a221625925e0d159e47b1a22782528824dfa1ac1
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 15:36:02 +0200

    Update BE.py

--- routes/calcul/BE.py
+++ routes/calcul/BE.py
@@ -9,19 +9,29 @@ db = client['test']
 
 
 @BE.route('/BE', methods=['GET'])
-def find_BE(data):
-    data = request.json
-    username = data.get('username')
-    collection_name = f"{username}_close"
-    collection = db[collection_name]
+def find_BE():
+    try:
+        data = request.json
+        username = data.get('username')
+        collection_name = f"{username}_close"
+        collection = db[collection_name]
 
-    resultBE = []
-    for order in collection.find({'type': 'Close'}):
-        rr = order.get('RR', None)
-        if rr is not None and -0.5 < rr < 0.5:
-            order['BE'] = True
-        else:
-            order['BE'] = False
-        resultBE.append(order)
+        resultBE = []
+        for order in collection.find({'type': 'Close'}).limit(100):  # Limiting to the first 100 results for testing
+            rr = order.get('RR', None)
+            if rr is not None and -0.5 < rr < 0.5:
+                order['BE'] = True
+            else:
+                order['BE'] = False
+            resultBE.append(order)
 
-    return resultBE
+        return jsonify(resultBE)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.register_blueprint(BE)
+    app.run()
