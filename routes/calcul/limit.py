033620071172000150eb802fb56371f1a0cd05a6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-01 23:40:50 +0200

    Create limit.py

---
+++ routes/calcul/limit.py
@@ -0,0 +1,20 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+
+
+limit = Blueprint('limit', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+def find_limit(data):
+    
+    orderType = data['orderType']
+    
+    if orderType ="BUY" or orderType ="SELL"
+        condi = False
+    else:
+        condi = True
+
+    return condi  # Renvoie la valeur de BE
