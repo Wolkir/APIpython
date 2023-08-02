ee534e7dcc24e17a958e9d092e9c01fe7c12671b
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-02 23:15:59 +0200

    Update bestrr.py

--- routes/calcul/bestrr.py
+++ routes/calcul/bestrr.py
@@ -11,7 +11,6 @@ db = client['test']
 
 @bestrr.route('/best', methods=['GET'])
 def calculate_best_rr():
-  
     collection = db["test2_close"]
 
     best_rr = 0
@@ -19,10 +18,10 @@ def calculate_best_rr():
     best_symbol = ""
     best_order_type = ""
 
-    rr_by_combination = {}  # Dictionnaire pour stocker la moyenne des RR pour chaque combinaison (Day, symbol, orderType)
-    rr_values = {}  # Dictionnaire pour stocker toutes les valeurs de RR pour chaque combinaison
+    rr_by_combination = {}  # Dictionnaire pour stocker la somme des RR pour chaque combinaison (Day, symbol, orderType)
+    rr_count_combination = {}  # Dictionnaire pour stocker le nombre de RR pour chaque combinaison
 
-    # Parcourir les documents de la collection et calculer la moyenne des RR pour chaque combinaison
+    # Parcourir les documents de la collection et calculer la somme des RR pour chaque combinaison
     for doc in collection.find():
         rr_value = doc.get('RR', 0)
         day = doc.get('Day')
@@ -30,11 +29,12 @@ def calculate_best_rr():
         order_type = doc.get('orderType')
         combination = (day, symbol, order_type)
 
-        rr_by_combination[combination] = rr_by_combination.get(combination, 0) + rr_value
-        rr_values[combination] = rr_values.get(combination, []) + [rr_value]
+        if rr_value is not None:
+            rr_by_combination[combination] = rr_by_combination.get(combination, 0) + rr_value
+            rr_count_combination[combination] = rr_count_combination.get(combination, 0) + 1
 
     for combination, rr_total in rr_by_combination.items():
-        rr_count = len(rr_values[combination])
+        rr_count = rr_count_combination.get(combination, 0)
         average_rr = rr_total / rr_count if rr_count > 0 else 0
 
         if average_rr > best_rr:
