10f8df4035217d6c1c178aa4b2e5dd0f26fcf12d
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-03 00:53:46 +0200

    Create totaltrade.py

---
+++ routes/calcul/totaltrade.py
@@ -0,0 +1,40 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+from datetime import timedelta
+
+app = Flask(__name__)
+averagegainloss = Blueprint('totaltrade', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+@averagegainloss.route('/totaltrade', methods=['GET'])
+def calculate_totaltrade(data):
+
+    username = data.get('username')
+    collection_name = f"{username}_close"
+ 
+    collection = db[collection_name]
+
+    # Obtenir tous les trades de la collection triés par ordre chronologique
+    trades = collection.find().sort("timestamp", 1)
+
+    # Numéro de position initialisé à 1
+    position_number = 1
+
+    # Parcourir chaque trade et lui attribuer un numéro de position
+    for trade in trades:
+        # Mettre à jour le trade avec le numéro de position
+        trade['position'] = position_number
+
+        # Incrémenter le numéro de position pour le prochain trade
+        position_number += 1
+
+        # Mettre à jour le trade dans la collection MongoDB
+        collection.update_one({'_id': trade['_id']}, {'$set': trade})
+
+    return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
