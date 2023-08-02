8fe11038bf7b0ca7fb7384405cdf41f9926c3820
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-02 14:06:18 +0200

    Update winrategroup.py

--- routes/calcul/winrategroup.py
+++ routes/calcul/winrategroup.py
@@ -7,7 +7,6 @@ winrategroup = Blueprint('winrategroup', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-
 @winrategroup.route('/winrategroup', methods=['GET'])
 def calculate_winrate_group(data):
     username = data.get('username')
@@ -16,10 +15,10 @@ def calculate_winrate_group(data):
     collection_name = f"{username}_close"
     collection_unitaire = f"{username}_unitaire"
     collection = db[collection_name]
-    
+
     # Récupérer tous les documents
     documents = list(collection.find())
-    
+
     positive_profits_count = 0
     negative_profits_count = 0
     positivelong_profits_count = 0
@@ -37,32 +36,32 @@ def calculate_winrate_group(data):
     # Compter le nombre de documents avec profit > 0 et les identifiants uniques
     # pour le calcul du winrate standard et du winrate real
     for doc in documents:
-    profit = doc['profit']
-    identifier = doc['identifier']
-    is_buy_order = orderType == "BUY"
-    is_sell_order = orderType == "SELL"
+        profit = doc['profit']
+        identifier = doc['identifier']
+        is_buy_order = orderType == "BUY"
+        is_sell_order = orderType == "SELL"
 
-    if profit > 0 and identifier not in positive_identifiers:
-        positive_profits_count += 1
-        positive_identifiers.add(identifier)
+        if profit > 0 and identifier not in positive_identifiers:
+            positive_profits_count += 1
+            positive_identifiers.add(identifier)
 
-        if is_buy_order and identifier not in positivelong_identifiers:
-            positivelong_profits_count += 1
-            positivelong_identifiers.add(identifier)
-        if is_sell_order and identifier not in positiveshort_identifiers:
-            positiveshort_profits_count += 1
-            positiveshort_identifiers.add(identifier)
+            if is_buy_order and identifier not in positivelong_identifiers:
+                positivelong_profits_count += 1
+                positivelong_identifiers.add(identifier)
+            if is_sell_order and identifier not in positiveshort_identifiers:
+                positiveshort_profits_count += 1
+                positiveshort_identifiers.add(identifier)
 
-    elif profit < 0 and identifier not in negative_identifiers:
-        negative_profits_count += 1
-        negative_identifiers.add(identifier)
+        elif profit < 0 and identifier not in negative_identifiers:
+            negative_profits_count += 1
+            negative_identifiers.add(identifier)
 
-        if is_buy_order and identifier not in negativelong_identifiers:
-            negativelong_profits_count += 1
-            negativelong_identifiers.add(identifier)
-        if is_sell_order and identifier not in negativeshort_identifiers:
-            negativeshort_profits_count += 1
-            negativeshort_identifiers.add(identifier)
+            if is_buy_order and identifier not in negativelong_identifiers:
+                negativelong_profits_count += 1
+                negativelong_identifiers.add(identifier)
+            if is_sell_order and identifier not in negativeshort_identifiers:
+                negativeshort_profits_count += 1
+                negativeshort_identifiers.add(identifier)
 
     # Calcul du winrate standard
     winratestd = positive_profits_count / (positive_profits_count + negative_profits_count) * 100
