d3e2f9b2973ad5991bfcfd8c33c763b58ec17f18
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 00:36:10 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -17,27 +17,23 @@ def calculate_totaltrade(data):
     collection_unitaire = f"{username}_unitaire"
     collection = db[collection_name]
 
-    # Obtenir le dernier trade de la collection triée par ordre chronologique
-    last_trade = collection.find_one(sort=[('timestamp', -1)])
+    # Obtenir tous les trades de la collection triée par ordre chronologique
+    trades = list(collection.find().sort('timestamp', 1))
 
-    # Compter le nombre total de trades dans la collection
-    total_trades = collection.count_documents({})
+    # Si la collection est vide, retourner un message
+    if not trades:
+        return jsonify({'message': 'Aucun trade dans la collection.'})
 
-    # Si la collection est vide, le numéro de position sera 1
-    if total_trades == 0:
-        total_trades = 1
+    # La première trade aura toujours totaltrade égal à 1
+    trades[0]['totaltrade'] = 1
 
-    # Ajouter le numéro de position pour le dernier trade ajouté à la collection
-    if last_trade:
-        # Si ce n'est pas le premier trade, récupérer la valeur de totaltrade du dernier trade et incrémenter de 1
-        if total_trades > 1:
-            total_trades = last_trade.get('totaltrade', 0) + 1
-            last_trade['totaltrade'] = total_trades
-            collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
-        else:
-            # Si c'est le premier trade, ne pas incrémenter et définir à 1
-            last_trade['totaltrade'] = 1
-            collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+    # Calculer le totaltrade pour les trades suivants
+    for i in range(1, len(trades)):
+        trades[i]['totaltrade'] = trades[i-1]['totaltrade'] + 1
+
+    # Mettre à jour tous les trades dans la collection
+    for trade in trades:
+        collection.update_one({'_id': trade['_id']}, {'$set': trade})
 
     return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
 
