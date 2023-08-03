1c6a979f2f4ba7e526a5c72c7eb81ce4ed34cd31
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 00:43:37 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -1,3 +1,14 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+from datetime import timedelta, datetime
+
+app = Flask(__name__)
+totaltrade = Blueprint('totaltrade', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
 @totaltrade.route('/totaltrade', methods=['GET'])
 def calculate_totaltrade(data):
 
@@ -21,7 +32,11 @@ def calculate_totaltrade(data):
 
     # Insérer le numéro de position pour le nouveau trade dans la collection
     # Notez qu'ici, nous n'essayons pas de mettre à jour les trades précédents
-    new_trade = {'totaltrade': total_trades, 'timestamp': current_timestamp, ...}  # Les détails du nouveau trade ici
+    current_timestamp = datetime.now()
+    new_trade = {'totaltrade': total_trades, 'timestamp': current_timestamp}  # Les détails du nouveau trade ici
     collection.insert_one(new_trade)
 
     return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
