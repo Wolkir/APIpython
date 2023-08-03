75069a5035d5496586017b9b3e812c6b1e1093dd
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 00:41:40 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -1,14 +1,3 @@
-from flask import Flask, Blueprint, jsonify
-from pymongo import MongoClient
-from datetime import timedelta
-
-app = Flask(__name__)
-totaltrade = Blueprint('totaltrade', __name__)
-
-# Connexion à la base de données MongoDB
-client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
-db = client['test']
-
 @totaltrade.route('/totaltrade', methods=['GET'])
 def calculate_totaltrade(data):
 
@@ -20,22 +9,19 @@ def calculate_totaltrade(data):
     # Obtenir le dernier trade de la collection triée par ordre chronologique
     last_trade = collection.find_one(sort=[('timestamp', -1)])
 
-    # Compter le nombre total de trades dans la collection
-    total_trades = collection.count_documents({})
-
-    if total_trades == 0:
-        # Aucun trade dans la collection, le numéro de position sera 1
-        total_trades = 1
-    else:
-        # Récupérer la valeur de totaltrade du dernier trade et ajouter 1 pour le nouveau trade
-        total_trades = last_trade.get('totaltrade', 0) + 1
-
-    # Ajouter le numéro de position pour le dernier trade ajouté à la collection
     if last_trade:
-        last_trade['totaltrade'] = total_trades
-        collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+        # Si un dernier trade existe, obtenir le numéro de position actuel
+        total_trades = last_trade.get('totaltrade', 0)
+    else:
+        # Aucun trade dans la collection, initialiser le numéro de position à 0
+        total_trades = 0
+
+    # Ajouter 1 pour le nouveau trade
+    total_trades += 1
+
+    # Insérer le numéro de position pour le nouveau trade dans la collection
+    # Notez qu'ici, nous n'essayons pas de mettre à jour les trades précédents
+    new_trade = {'totaltrade': total_trades, 'timestamp': current_timestamp, ...}  # Les détails du nouveau trade ici
+    collection.insert_one(new_trade)
 
     return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
-
-if __name__ == "__main__":
-    app.run(debug=True)
