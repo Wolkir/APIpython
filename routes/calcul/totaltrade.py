1741fe66cfc27d650c1aea439b10c279cc0b8acb
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 00:29:11 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -17,23 +17,22 @@ def calculate_totaltrade(data):
     collection_unitaire = f"{username}_unitaire"
     collection = db[collection_name]
 
-    # Obtenir le dernier trade de la collection triée par ordre chronologique
-    last_trade = collection.find_one(sort=[('timestamp', -1)])
-
     # Compter le nombre total de trades dans la collection
     total_trades = collection.count_documents({})
 
+    # Si la collection est vide, le numéro de position sera 1
     if total_trades == 0:
-        # Aucun trade dans la collection, le numéro de position sera 1
         total_trades = 1
-    else:
-        # Récupérer la valeur de totaltrade du dernier trade et ajouter 1 pour le nouveau trade
-        total_trades = last_trade.get('totaltrade', 0) + 1
+
+    # Obtenir le dernier trade de la collection triée par ordre chronologique
+    last_trade = collection.find_one(sort=[('timestamp', -1)])
 
     # Ajouter le numéro de position pour le dernier trade ajouté à la collection
     if last_trade:
-        last_trade['totaltrade'] = total_trades
-        collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+        # Vérifier si ce n'est pas le premier trade, alors incrémenter le numéro de position
+        if total_trades > 1:
+            last_trade['totaltrade'] = total_trades
+            collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
 
     return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
 
