cb25f8e6402708507482b9817565d2e12a89e2e7
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 16:27:17 +0200

    Update and rename routes/totaltradeok.py to routes/calcul/totaltradeok.py

---
+++ routes/calcul/totaltradeok.py
@@ -0,0 +1,42 @@
+
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+from datetime import timedelta
+
+app = Flask(__name__)
+totaltrade = Blueprint('totaltrade', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+@totaltrade.route('/totaltrade', methods=['GET'])
+def calculate_totaltrade(data):
+
+    username = data.get('username')
+    collection_name = f"{username}_close"
+    collection_unitaire = f"{username}_unitaire"
+    collection = db[collection_name]
+
+    # Obtenir le dernier trade de la collection triée par ordre chronologique
+    last_trade = collection.find_one(sort=[('timestamp', -1)])
+
+    # Compter le nombre total de trades dans la collection
+    total_trades = collection.count_documents({})
+
+    if total_trades == 0:
+        # Aucun trade dans la collection, le numéro de position sera 1
+        total_trades = 1
+    else:
+        # Récupérer la valeur de totaltrade du dernier trade et ajouter 1 pour le nouveau trade
+        total_trades = last_trade.get('totaltrade', 0) + 1
+
+    # Ajouter le numéro de position pour le dernier trade ajouté à la collection
+    if last_trade:
+        last_trade['totaltrade'] = total_trades
+        collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+
+    return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
