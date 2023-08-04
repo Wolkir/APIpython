bbf1f0598f884d8510c340259c78859d70148bcf
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-04 21:25:56 +0100

    Update modificationTrade.py

--- routes/journal/modificationTrade.py
+++ routes/journal/modificationTrade.py
@@ -40,9 +40,107 @@ def setup_modificationTrade_routes(app):
             timeSetup_data = data.get('timeSetup', [])
             porteFeuille_data = data.get('porteFeuille', [])
             collection_data = data.get('collection', [])
-            things_collection = mongo.db.things
+            things_collection = mongo.db[collection_data]
 
             reinsertion = []
+
+            # Mise à jour ou création des champs psychologie
+            for psychologie_item in psychologie_data:
+                trade_id = psychologie_item.get('id')
+                value_psy = psychologie_item.get('valuePsy')
+
+                if trade_id and value_psy:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'psychologie': value_psy}}, upsert=True)
+            
+            # Mise à jour du champ annonceEconomique
+            for trade in trades_data:
+                trade_id = trade.get('id')
+                valeur_ann_eco = trade.get('valeurAnnEco')
+
+                if trade_id and valeur_ann_eco in ['oui', 'non']:
+                    annonce_economique = True if valeur_ann_eco == 'oui' else False
+
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'annonceEconomique': annonce_economique}})
+            # Mise à jour du champ position
+            for position in position_data:
+                trade_id = position.get('id')
+                valeur_position = position.get('valuePosition')
+
+                if trade_id and valeur_position:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'position': valeur_position}})
+            
+            # Mise à jour du champ type ordre
+            for typeOrdre in typeOrdre_data:
+                trade_id = typeOrdre.get('id')
+                valeur_typeOrdre = typeOrdre.get('valueTypeOrdre')
+
+                if trade_id and valeur_typeOrdre:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'typeOrdre': valeur_typeOrdre}})
+
+            # Mise à jour du champ violeStrategie
+            for viole in violeStrategie_data:
+                trade_id = viole.get('id')
+                valeur_violeStrategie = viole.get('valueVioleStrategie')
+
+                if trade_id and valeur_violeStrategie in ['oui', 'non']:
+                    violeStrategie = True if valeur_violeStrategie == 'oui' else False
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'violeStrategie': violeStrategie}})
+            
+            # Mise à jour du champ sortie
+            for sortie in sortie_data:
+                trade_id = sortie.get('id')
+                valeur_sortie = sortie.get('valueSortie')
+
+                if trade_id and valeur_sortie:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'sortie': valeur_sortie}})
+
+            # Mise à jour ou création des champs indicateur1
+            for indicateur1_item in indicateur1_data:
+                trade_id = indicateur1_item.get('id')
+                value_indicateur1 = indicateur1_item.get('valueIndicateur1')
+
+                if trade_id and value_indicateur1:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur1': value_indicateur1}}, upsert=True)
+
+            # Mise à jour ou création des champs indicateur2
+            for indicateur2_item in indicateur2_data:
+                trade_id = indicateur2_item.get('id')
+                value_indicateur2 = indicateur2_item.get('valueIndicateur2')
+
+                if trade_id and value_indicateur2:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur2': value_indicateur2}}, upsert=True)
+
+            # Mise à jour ou création des champs indicateur3
+            for indicateur3_item in indicateur3_data:
+                trade_id = indicateur3_item.get('id')
+                value_indicateur3 = indicateur3_item.get('valueIndicateur3')
+
+                if trade_id and value_indicateur3:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur3': value_indicateur3}}, upsert=True)
+
+            # Mise à jour ou création des champs strategie
+            for strategie_item in strategie_data:
+                trade_id = strategie_item.get('id')
+                value_strategie = strategie_item.get('valueStrategie')
+
+                if trade_id and value_strategie:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'strategie': value_strategie}}, upsert=True)
+
+            # Mise à jour ou création des champs timeEntree
+            for timeEntree_item in timeEntree_data:
+                trade_id = timeEntree_item.get('id')
+                value_timeEntree = timeEntree_item.get('valueTimeEntree')
+
+                if trade_id and value_timeEntree:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'timeEntree': value_timeEntree}}, upsert=True)
+
+            # Mise à jour ou création des champs timeSetup
+            for timeSetup_item in timeSetup_data:
+                trade_id = timeSetup_item.get('id')
+                value_timeSetup = timeSetup_item.get('valueTimeSetup')
+
+                if trade_id and value_timeSetup:
+                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'timeSetup': value_timeSetup}}, upsert=True)
 
             # transfert trade
             if porteFeuille_data is not None:
@@ -59,190 +157,6 @@ def setup_modificationTrade_routes(app):
                             line = {'trade_id': trade_id, 'value_porteFeuille': value_porteFeuille}
                             reinsertion.append(line)
 
-            # Mise à jour ou création des champs psychologie
-            for psychologie_item in psychologie_data:
-                trade_id = psychologie_item.get('id')
-                value_psy = psychologie_item.get('valuePsy')
-
-                if trade_id and value_psy:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'psychologie': value_psy}}, upsert=True)
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_psy'] = value_psy
-                    else:
-                        line = {'trade_id': trade_id, 'value_psy': value_psy}
-                        reinsertion.append(line)
-            
-            # Mise à jour du champ annonceEconomique
-            for trade in trades_data:
-                trade_id = trade.get('id')
-                valeur_ann_eco = trade.get('valeurAnnEco')
-
-                if trade_id and valeur_ann_eco in ['oui', 'non']:
-                    annonce_economique = True if valeur_ann_eco == 'oui' else False
-
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'annonceEconomique': annonce_economique}})
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['valeur_ann_eco'] = valeur_ann_eco
-                    else:
-                        line = {'trade_id': trade_id, 'valeur_ann_eco': valeur_ann_eco}
-                        reinsertion.append(line)
-            # Mise à jour du champ position
-            for position in position_data:
-                trade_id = position.get('id')
-                valeur_position = position.get('valuePosition')
-
-                if trade_id and valeur_position:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'position': valeur_position}})
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['valeur_position'] = valeur_position
-                    else:
-                        line = {'trade_id': trade_id, 'valeur_position': valeur_position}
-            
-            # Mise à jour du champ type ordre
-            for typeOrdre in typeOrdre_data:
-                trade_id = typeOrdre.get('id')
-                valeur_typeOrdre = typeOrdre.get('valueTypeOrdre')
-
-                if trade_id and valeur_typeOrdre:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'typeOrdre': valeur_typeOrdre}})
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['valeur_typeOrdre'] = valeur_typeOrdre
-                    else:
-                        line = {'trade_id': trade_id, 'valeur_typeOrdre': valeur_typeOrdre}
-
-            # Mise à jour du champ violeStrategie
-            for viole in violeStrategie_data:
-                trade_id = viole.get('id')
-                valeur_violeStrategie = viole.get('valueVioleStrategie')
-
-                if trade_id and valeur_violeStrategie in ['oui', 'non']:
-                    violeStrategie = True if valeur_violeStrategie == 'oui' else False
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'violeStrategie': violeStrategie}})
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['valeur_violeStrategie'] = valeur_violeStrategie
-                    else:
-                        line = {'trade_id': trade_id, 'valeur_violeStrategie': valeur_violeStrategie}
-            
-            # Mise à jour du champ sortie
-            for sortie in sortie_data:
-                trade_id = sortie.get('id')
-                valeur_sortie = sortie.get('valueSortie')
-
-                if trade_id and valeur_sortie:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'sortie': valeur_sortie}})
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['valeur_sortie'] = valeur_sortie
-                    else:
-                        line = {'trade_id': trade_id, 'valeur_sortie': valeur_sortie}
-
-            # Mise à jour ou création des champs indicateur1
-            for indicateur1_item in indicateur1_data:
-                trade_id = indicateur1_item.get('id')
-                value_indicateur1 = indicateur1_item.get('valueIndicateur1')
-
-                if trade_id and value_indicateur1:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur1': value_indicateur1}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_indicateur1'] = value_indicateur1
-                    else:
-                        line = {'trade_id': trade_id, 'value_indicateur1': value_indicateur1}
-
-            # Mise à jour ou création des champs indicateur2
-            for indicateur2_item in indicateur2_data:
-                trade_id = indicateur2_item.get('id')
-                value_indicateur2 = indicateur2_item.get('valueIndicateur2')
-
-                if trade_id and value_indicateur2:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur2': value_indicateur2}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_indicateur2'] = value_indicateur2
-                    else:
-                        line = {'trade_id': trade_id, 'value_indicateur2': value_indicateur2}
-
-            # Mise à jour ou création des champs indicateur3
-            for indicateur3_item in indicateur3_data:
-                trade_id = indicateur3_item.get('id')
-                value_indicateur3 = indicateur3_item.get('valueIndicateur3')
-
-                if trade_id and value_indicateur3:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'indicateur3': value_indicateur3}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_indicateur3'] = value_indicateur3
-                    else:
-                        line = {'trade_id': trade_id, 'value_indicateur3': value_indicateur3}
-
-            # Mise à jour ou création des champs strategie
-            for strategie_item in strategie_data:
-                trade_id = strategie_item.get('id')
-                value_strategie = strategie_item.get('valueStrategie')
-
-                if trade_id and value_strategie:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'strategie': value_strategie}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_strategie'] = value_strategie
-                    else:
-                        line = {'trade_id': trade_id, 'value_strategie': value_strategie}
-
-            # Mise à jour ou création des champs timeEntree
-            for timeEntree_item in timeEntree_data:
-                trade_id = timeEntree_item.get('id')
-                value_timeEntree = timeEntree_item.get('valueTimeEntree')
-
-                if trade_id and value_timeEntree:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'timeEntree': value_timeEntree}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_timeEntree'] = value_timeEntree
-                    else:
-                        line = {'trade_id': trade_id, 'value_timeEntree': value_timeEntree}
-
-            # Mise à jour ou création des champs timeSetup
-            for timeSetup_item in timeSetup_data:
-                trade_id = timeSetup_item.get('id')
-                value_timeSetup = timeSetup_item.get('valueTimeSetup')
-
-                if trade_id and value_timeSetup:
-                    things_collection.update_one({'_id': ObjectId(trade_id)}, {'$set': {'timeSetup': value_timeSetup}}, upsert=True)
-
-                    existing_line = next((line for line in reinsertion if line['trade_id'] == trade_id), None)
-                    
-                    if existing_line:
-                        existing_line['value_timeSetup'] = value_timeSetup
-                    else:
-                        line = {'trade_id': trade_id, 'value_timeSetup': value_timeSetup}
-
-            print(reinsertion)
-            print(collection_data)
             def fonctionDeReinsertion(reinsertion, collection_data):
                 mongo = PyMongo(current_app)
                 db = mongo.db
