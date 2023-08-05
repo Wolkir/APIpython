f877e9c18b908f809a2fb5d365f8a4aef1139fd8
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-05 23:45:09 +0200

    Update tradercount.py

--- routes/calcul/tradercount.py
+++ routes/calcul/tradercount.py
@@ -1,35 +1,41 @@
-from flask import Flask, Blueprint, jsonify, request
+from flask import Flask, Blueprint, jsonify
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import timedelta
 
-tradercount = Blueprint('tradercount', __name__)
+app = Flask(__name__)
+totaltrade = Blueprint('totaltrade', __name__)
 
+# Connexion à la base de données MongoDB
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
-# Variable globale pour stocker le tradercount de la journée en cours
-daily_trade_counts = {}
 
-@tradercount.route('/tradercount', methods=['GET'])
-@tradercount.route('/tradercount', methods=['GET'])
-def calculate_tradercount(data):
-    try:
-        username = data.get('username')
+@totaltrade.route('/totaltrade', methods=['GET'])
+def calculate_totaltrade(data):
 
-        collection_close = f"{username}_close"
+    username = data.get('username')
+    collection_name = f"{username}_close"
+    collection_unitaire = f"{username}_unitaire"
+    collection = db[collection_name]
 
-        # Rechercher le dernier trade (le trade avec le tradecount le plus élevé)
-        last_trade = db[collection_close].find_one(sort=[('tradecount', -1)])
+    # Obtenir le dernier trade de la collection triée par ordre chronologique
+    last_trade = collection.find_one(sort=[('timestamp', -1)])
 
-        # Si aucun trade n'a été fait jusqu'à présent, alors le tradecount sera 1 pour le nouveau trade
-        if not last_trade:
-            tradecount = 1
-        else:
-            # Sinon, le tradecount du nouveau trade sera le tradecount du dernier trade + 1
-            tradecount = last_trade['tradecount'] + 1
+    # Compter le nombre total de trades dans la collection
+    total_trades = collection.count_documents({})
 
-        print("TradeCount:", tradecount)
-        return str(tradecount)
+    if total_trades == 0:
+        # Aucun trade dans la collection, le numéro de position sera 1
+        total_trades = 1
+    else:
+        # Récupérer la valeur de totaltrade du dernier trade et ajouter 1 pour le nouveau trade
+        total_trades = last_trade.get('totaltrade', 0) + 1
 
-    except Exception as e:
-        print("Error:", e)
-        return jsonify({'error': str(e)}), 500
+    # Ajouter le numéro de position pour le dernier trade ajouté à la collection
+    if last_trade:
+        last_trade['totaltrade'] = total_trades
+        collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+
+    return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
