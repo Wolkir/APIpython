8a7a447b289ed547213b69e7d73944b29db7cfb0
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-06 00:01:36 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -11,31 +11,24 @@ db = client['test']
 
 @totaltrade.route('/totaltrade', methods=['GET'])
 def calculate_totaltrade(data):
+    try:
+        username = data.get('username')
 
-    username = data.get('username')
-    collection_name = f"{username}_close"
-    collection_unitaire = f"{username}_unitaire"
-    collection = db[collection_name]
+        collection_close = f"{username}_close"
 
-    # Obtenir le dernier trade de la collection triée par ordre chronologique
-    last_trade = collection.find_one(sort=[('timestamp', -1)])
+        # Rechercher le dernier trade (le trade avec le totaltrade le plus élevé)
+        last_trade = db[collection_close].find_one(sort=[('totaltrade', -1)])
 
-    # Compter le nombre total de trades dans la collection
-    total_trades = collection.count_documents({})
+        # Si aucun trade n'a été fait jusqu'à présent, alors le totaltrade sera 1 pour le nouveau trade
+        if not last_trade:
+            totaltrade = 1
+        else:
+            # Sinon, le totaltrade du nouveau trade sera le totaltrade du dernier trade + 1
+            totaltrade = last_trade['totaltrade'] + 1
 
-    if total_trades == 0:
-        # Aucun trade dans la collection, le numéro de position sera 1
-        total_trades = 1
-    else:
-        # Récupérer la valeur de totaltrade du dernier trade et ajouter 1 pour le nouveau trade
-        total_trades = last_trade.get('totaltrade', 0) + 1
+        print("totaltrade:", totaltrade)
+        return str(totaltrade)
 
-    # Ajouter le numéro de position pour le dernier trade ajouté à la collection
-    if last_trade:
-        last_trade['totaltrade'] = total_trades
-        collection.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
-
-    return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'error': str(e)}), 500
