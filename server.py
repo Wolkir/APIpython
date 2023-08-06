c2eba0ec10c336a7cf7dcf198f66b209d30a993f
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-07 00:09:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ from routes.calcul.winrrtflat import winrrtflat
 from routes.calcul.BE import BE
 from routes.calcul.limit import limit
 from routes.calcul.bestrr import bestrr
-from routes.calcul.average.averagetrade import averagetrade
+#from routes.calcul.average.averagetrade import averagetrade
 from routes.calcul.daytrade import daytrade
 from routes.calcul.totaltrade import totaltrade
 
@@ -136,7 +136,7 @@ app.register_blueprint(winrrtflat)
 #app.register_blueprint(tradercount)
 app.register_blueprint(limit)
 app.register_blueprint(bestrr)
-app.register_blueprint(averagetrade)
+#app.register_blueprint(averagetrade)
 app.register_blueprint(daytrade)
 app.register_blueprint(totaltrade)
 
