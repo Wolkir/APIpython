2c48a1691422f4a728b96d6d648c088ee563b5c5
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-06 16:20:54 +0200

    Update server.py

--- server.py
+++ server.py
@@ -161,7 +161,7 @@ app.register_blueprint(totaltrade)
 #app.register_blueprint(setup_user_routes(app))
 
 # recuperation trade de mt5
-app.register_blueprint(trade_blueprint)
+#app.register_blueprint(trade_blueprint)
 
 # strategie
 #app.register_blueprint(recuperationStrategie)
@@ -169,7 +169,7 @@ app.register_blueprint(trade_blueprint)
 #app.register_blueprint(suppressionStrategie)
 
 # porteFeuille
-"app.register_blueprint(recuperationPorteFeuille)
+#app.register_blueprint(recuperationPorteFeuille)
 #app.register_blueprint(createPorteFeuille)
 #app.register_blueprint(suppressionPorteFeuille)
 
