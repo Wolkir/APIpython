b19d40d8f8c74401687c0525402b57d768fb5680
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 01:13:18 +0200

    Update sortiemanu.py

--- routes/calcul/sortiemanu.py
+++ routes/calcul/sortiemanu.py
@@ -13,24 +13,21 @@ sortiemanu = Blueprint('sortiemanu', __name__)
 @sortiemanu.route('/sortiemanu', methods=['GET'])
 
 def calculate_sortiemanu(data):
-
-
     username = data.get('username')
     collection_name = f"{username}_close"
     collection = db[collection_name]
     
     closurePosition = data.get('closurePosition')
-    TPR = data.get('TPR')
-    SLR = data.get('SLR')
+    
+    # Utilisation des fonctions pour obtenir TPR et SLR
+    TPR = calculate_tpr(data)
+    SLR = calculate_slr(data)
 
-    if closurePosition == 'Close' and TPR == False and SLR == False :
+    if closurePosition == 'Close' and not TPR and not SLR:
         Smanu = True
     else:
         Smanu = False
 
     return jsonify({'Smanu': Smanu})
-
-
-
 
 
