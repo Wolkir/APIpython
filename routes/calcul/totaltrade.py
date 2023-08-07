424c5a23ad7ed07f34b64e8505aa87b942161551
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 01:53:38 +0200

    Update totaltrade.py

--- routes/calcul/totaltrade.py
+++ routes/calcul/totaltrade.py
@@ -55,7 +55,7 @@ def calculate_totaltrade(data):
 
     last_trade['_id'] = ObjectId()  # Générer un nouvel ID pour éviter des doublons
     last_trade['total_trade'] = total_trades
-    collection_unitaire.update_one({'_id': last_trade['_id']}, {'$set': last_trade})
+    collection_unitaire.update_many({}, {'$set': {'total_trade': last_trade}})
     
     return jsonify({'message': 'Numéro de position ajouté à chaque trade avec succès.'})
 
