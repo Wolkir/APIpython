8f4c14b4828ba851a6d8e06a38f9cf9f0ed56fc6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 13:32:15 +0200

    Update sortiemanu.py

--- routes/calcul/sortiemanu.py
+++ routes/calcul/sortiemanu.py
@@ -3,25 +3,25 @@ from pymongo import MongoClient
 from routes.calcul.TPR import calculate_tpr
 from routes.calcul.SLR import calculate_slr
 from bson import ObjectId
+
 app = Flask(__name__)
 
-# Connexion à la base de données MongoDB
+# Connection to MongoDB database
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-# Blueprint pour /sortiemanu
 sortiemanu = Blueprint('sortiemanu', __name__)
 
-@sortiemanu.route('/sortiemanu', methods=['GET'])
-
+@app.route('/sortiemanu', methods=['GET'])
 def calculate_sortiemanu(data):
+    data = request.args
     username = data.get('username')
     collection_name = f"{username}_close"
     collection = db[collection_name]
     
     closurePosition = data.get('closurePosition')
     
-    # Utilisation des fonctions pour obtenir TPR et SLR
+    # Use functions to get TPR and SLR
     TPR = calculate_tpr(data)
     SLR = calculate_slr(data)
 
@@ -32,4 +32,8 @@ def calculate_sortiemanu(data):
 
     return jsonify({'Smanu': Smanu})
 
+# Register the blueprint
+app.register_blueprint(sortiemanu)
 
+if __name__ == '__main__':
+    app.run()
