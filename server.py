481c20fc119431532e21362f48d7a78999bd1ae4
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 01:23:06 +0100

    Update server.py

--- server.py
+++ server.py
@@ -188,7 +188,7 @@ app.register_blueprint(envoie)
 # journal
 app.register_blueprint(setup_things_routes(app))
 app.register_blueprint(setup_modificationTrade_routes(app))
-app.register_blueprint(delete_suppressionTrade(app))
+app.register_blueprint(delete_suppressionTrade)
 
 # image
 app.register_blueprint(enregistrerImage)
