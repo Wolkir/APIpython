cc1e23ff3238463df522a409479897ade744ed1d
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 23:42:00 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -172,11 +172,17 @@ def save_trade_request():
             total_trade = calculate_totaltrade(data)
             data['totaltrade'] = total_trade
 
+            data['volume'] = round(data.get('volume'), 2)
+            volume_remain = round(volume_remain, 2)
+
             #Smanu = calculate_sortiemanu(data)
             #data['Sortiemanu'] = Smanu
 
             #position = calculate_totaltrade(data)
             #data['totaltrade'] = position
+
+            data['volume'] = round(data.get('volume'), 2)
+            volume_remain = round(volume_remain, 2)
 
           
 
@@ -231,7 +237,7 @@ def save_trade_request():
             "typeOfTransaction": data.get('typeOfTransaction'),
             "orderType": data.get('orderType'),
             "volume": data.get('volume'),
-            #"volume_remain": volume_remain,
+            "volume_remain": data.get('volume_remain'),
             "symbol": data.get('symbole'),
             "priceOpening": data.get('priceOpening'),
             "stopLoss": data.get('stopLoss'),
