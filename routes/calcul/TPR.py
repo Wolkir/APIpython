64b45f853a0912920fc2d3b399d94bd038022856
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-08 22:56:47 +0200

    Update TPR.py

--- routes/calcul/TPR.py
+++ routes/calcul/TPR.py
@@ -1,1 +1,23 @@
+from flask import Blueprint, jsonify, request
+from pymongo import MongoClient
 
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+
+tprbp = Blueprint('tpr', __name__)
+
+def calculate_tpr(data):
+    # Your TPR calculation logic here based on the 'entry' data
+    # For example:
+    orderType = data.get('orderType')
+    price_closure = data.get('priceClosure')
+    take_profit = data.get('takeProfit')
+    
+    if orderType == "BUY" and price_closure >= take_profit and take_profit>0:
+        entry['TPR'] = True
+    elif orderType == "SELL" and price_closure <= take_profit and take_profit>0 :
+        entry['TPR'] = True
+    else:
+        entry['TPR'] = False
+
+    return entry
