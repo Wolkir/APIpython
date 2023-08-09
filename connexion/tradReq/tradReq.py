f4d4a671789020fca8bb6417f3498367b8ec45ae
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-09 12:08:19 +0200

    Update tradReq.py

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -191,8 +191,8 @@ def save_trade_request():
           
 
         # Round 'volume' and 'volume_remain' to two decimal places
-        data['volume'] = round(data.get('volume'), 2)
-        volume_remain = round(volume_remain, 2)
+        #data['volume'] = round(data.get('volume'), 2)
+        #volume_remain = round(volume_remain, 2)
 
         # Calculate killzone only for 'Open' orders
         if closure_position == "Open":
@@ -231,7 +231,7 @@ def save_trade_request():
             "typeOfTransaction": data.get('typeOfTransaction'),
             "orderType": data.get('orderType'),
             "volume": data.get('volume'),
-            "volume_remain": volume_remain,
+            #"volume_remain": volume_remain,
             "symbol": data.get('symbole'),
             "priceOpening": data.get('priceOpening'),
             "stopLoss": data.get('stopLoss'),
