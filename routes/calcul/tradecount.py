1f55ae5ece7dc684902a6d7296e40290dc903aa2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-10 23:11:39 +0200

    Update tradecount.py

--- routes/calcul/tradecount.py
+++ routes/calcul/tradecount.py
@@ -8,26 +8,27 @@ tradecount = Blueprint('tradecount', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-@tradecount.route('/tradecount', methods=['GET'])
-def calculate_tradecount(data):
+@tradecount.route('/tradecount', methods=['POST'])
+def calculate_tradecount():
     
-
+    data = request.json
     username = data.get('username')
-    # Récupérer la date sans l'heure, les minutes et les secondes
-    date_of_trade = datetime.strptime(data.get('dateAndTimeOpening'), '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
-    
+    raw_date = data.get('dateAndTimeOpening')
+    status = data.get('closurePosition')  # "open" ou "close"
+
+    if not raw_date:
+        return jsonify({"error": "Date not provided"}), 400
+
+    try:
+        date_of_trade = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
     collection_close = db[f"{username}_close"]
     collection_open = db[f"{username}_open"]
 
-    # Comptez les trades fermés et ouverts pour la date donnée
-    count_close = collection_close.count_documents({"date": {"$regex": f"^{date_of_trade}"}})
-    count_open = collection_open.count_documents({"date": {"$regex": f"^{date_of_trade}"}})
+    count_close = collection_close.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+    count_open = collection_open.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
 
-    # Calculer le numéro de trade pour le nouveau trade
     new_trade_number = count_close + count_open + 1
-
-    # À ce stade, vous pouvez soit ajouter ce nouveau trade à la collection appropriée, soit renvoyer ce numéro.
-    # Note: Si vous l'ajoutez à la collection, assurez-vous d'inclure le numéro dans le document de trade.
-
     return new_trade_number
-
