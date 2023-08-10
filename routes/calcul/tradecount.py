cdddb3db0c081091200cf7f95e16f3403ce2001b
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-10 23:50:45 +0200

    Update tradecount.py

--- routes/calcul/tradecount.py
+++ routes/calcul/tradecount.py
@@ -8,9 +8,9 @@ tradecount = Blueprint('tradecount', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-@tradecount.route('/tradecount', methods=['GET'])  # Modifier 'GET' à 'POST'
+
+@tradecount.route('/tradecount', methods=['GET'])
 def calculate_tradecount(data):
-    
     
     username = data.get('username')
     raw_date = data.get('dateAndTimeOpening')
@@ -21,24 +21,25 @@ def calculate_tradecount(data):
 
     try:
         date_of_trade = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
-    except ValueError:
-        return "Invalid date format", 400
+    except ValueError as e:
+        return str(e), 400
 
     collection_close = db[f"{username}_close"]
     collection_open = db[f"{username}_open"]
 
     if status == "open":
-        last_opened_trade = collection_open.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
-        if last_opened_trade:
-            new_trade_number = last_opened_trade["trade_number"] + 1
-        else:
-            last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
-            new_trade_number = last_closed_trade["trade_number"] + 1 if last_closed_trade else 1
+        count_close = collection_close.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+        count_open = collection_open.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+        new_trade_number = count_close + count_open + 1
 
     elif status == "close":
         last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
-        new_trade_number = last_closed_trade["trade_number"] + 1 if last_closed_trade else 1
+        if last_closed_trade:
+            new_trade_number = last_closed_trade["trade_number"] + 1
+        else:
+            new_trade_number = 1
 
     else:
-        return "Status not recognized", 400
+        return "Invalid status", 400
+
     return new_trade_number
