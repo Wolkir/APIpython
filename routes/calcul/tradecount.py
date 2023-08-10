1ee7f86ed88d91aea335582eb3edbc60c6fdc195
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-10 23:32:55 +0200

    Update tradecount.py

--- routes/calcul/tradecount.py
+++ routes/calcul/tradecount.py
@@ -8,13 +8,13 @@ tradecount = Blueprint('tradecount', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-@tradecount.route('/tradecount', methods=['GET'])
-
+@tradecount.route('/tradecount', methods=['POST'])  # Modifier 'GET' à 'POST'
 def calculate_tradecount(data):
+    
 
     username = data.get('username')
     raw_date = data.get('dateAndTimeOpening')
-    identifier = data.get('identifier')  # Je suppose que chaque trade a un ID unique pour le récupérer.
+    status = data.get('closurePosition')  # "open" ou "close"
 
     if not raw_date:
         return jsonify({"error": "Date not provided"}), 400
@@ -22,21 +22,22 @@ def calculate_tradecount(data):
     try:
         date_of_trade = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
     except ValueError as e:
-        
+        return jsonify({"error": str(e)}), 400
 
     collection_close = db[f"{username}_close"]
     collection_open = db[f"{username}_open"]
 
-    # Suppression du trade de la collection 'open'
-    trade_to_close = collection_open.find_one_and_delete({"_id": identifier})
-
-   
-    # Récupération du dernier trade_number dans la collection 'close'
-    last_closed_trade = collection_close.find_one(sort=[("trade_number", -1)])
-    if last_closed_trade:
-        new_trade_number = last_closed_trade["trade_number"] + 1
+    # Si le trade est ouvert, vérifiez simplement le total des trades ouverts et fermés.
+    if status == "open":
+        count_close = collection_close.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+        count_open = collection_open.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+        new_trade_number = count_close + count_open + 1
+    # Si le trade est fermé, obtenez le dernier `trade_number` de la collection `close` pour cette date spécifique et ajoutez 1.
     else:
-        new_trade_number = 1
+        last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
+        if last_closed_trade:
+            new_trade_number = last_closed_trade["trade_number"] + 1
+        else:
+            new_trade_number = 1
 
-   
-    return  new_trade_number
+    return new_trade_number
