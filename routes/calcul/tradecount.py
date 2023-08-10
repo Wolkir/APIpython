b5e784265280eed74f3ab9d79a1579e774d70591
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-10 23:39:47 +0200

    Update tradecount.py

--- routes/calcul/tradecount.py
+++ routes/calcul/tradecount.py
@@ -8,10 +8,9 @@ tradecount = Blueprint('tradecount', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-@tradecount.route('/tradecount', methods=['POST'])  # Modifier 'GET' à 'POST'
+@tradecount.route('/tradecount', methods=['GET'])  # Modifier 'GET' à 'POST'
 def calculate_tradecount(data):
     
-
     username = data.get('username')
     raw_date = data.get('dateAndTimeOpening')
     status = data.get('closurePosition')  # "open" ou "close"
@@ -27,17 +26,19 @@ def calculate_tradecount(data):
     collection_close = db[f"{username}_close"]
     collection_open = db[f"{username}_open"]
 
-    # Si le trade est ouvert, vérifiez simplement le total des trades ouverts et fermés.
     if status == "open":
-        count_close = collection_close.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
-        count_open = collection_open.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
-        new_trade_number = count_close + count_open + 1
-    # Si le trade est fermé, obtenez le dernier `trade_number` de la collection `close` pour cette date spécifique et ajoutez 1.
-    else:
-        last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
-        if last_closed_trade:
-            new_trade_number = last_closed_trade["trade_number"] + 1
+        last_opened_trade = collection_open.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
+        if last_opened_trade:
+            new_trade_number = last_opened_trade["trade_number"] + 1
         else:
-            new_trade_number = 1
+            last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
+            new_trade_number = last_closed_trade["trade_number"] + 1 if last_closed_trade else 1
+
+    elif status == "close":
+        last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
+        new_trade_number = last_closed_trade["trade_number"] + 1 if last_closed_trade else 1
+
+    else:
+        return jsonify({"error": "Status not recognized"}), 400
 
     return new_trade_number
