efe91db7d418ea2986727dd499550df2a3736db3
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-11 00:33:16 +0200

    Update tradecount.py

--- routes/calcul/tradecount.py
+++ routes/calcul/tradecount.py
@@ -15,30 +15,41 @@ def calculate_tradecount(data):
     username = data.get('username')
     raw_date = data.get('dateAndTimeOpening')
     status = data.get('closurePosition')  # "open" ou "close"
- 
+    trade_number = data.get('tradecount')
 
     if not raw_date:
-        return "Date not provided", 400
+        return jsonify({"error": "Date not provided"}), 400
 
     try:
         date_of_trade = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
     except ValueError as e:
-        return str(e), 400
+        return jsonify({"error": str(e)}), 400
 
     collection_close = db[f"{username}_close"]
     collection_open = db[f"{username}_open"]
 
+    multiple = False  # Initializing the multiple variable
+
     if status == "Open":
         count_close = collection_close.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
         count_open = collection_open.count_documents({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}})
+        
+        # Check if another order is already open
+        if count_open > 0:
+            multiple = True
+        
         new_trade_number = count_close + count_open + 1
+
     elif status == "Close":
-        last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("tradecount", -1)])
+        last_closed_trade = collection_close.find_one({"dateAndTimeOpening": {"$regex": f"^{date_of_trade}"}}, sort=[("trade_number", -1)])
         if last_closed_trade:
-            new_trade_number = last_closed_trade["tradecount"] + 1
+            new_trade_number = last_closed_trade["trade_number"] + 1
         else:
             new_trade_number = 1
     else:
-        return f"Invalid status value: {status}", 400
+        return jsonify({"error": f"Invalid status value: {status}"}), 400
 
-    return new_trade_number
+    return ({
+        'trade_number': new_trade_number,
+        'multiple': multiple
+    }), 200
