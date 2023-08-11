1b5da7e379466bd3f91d4d720a7ed76d42045206
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-11 21:32:59 +0100

    Update recuperationImage.py

--- routes/journal/recuperationImage.py
+++ routes/journal/recuperationImage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, current_app, Flask
+from flask import Blueprint, jsonify, send_file, current_app, request, Flask
 from pymongo import MongoClient
 from bson import ObjectId
 import gridfs
@@ -6,29 +6,22 @@ import io
 
 app = Flask(__name__)
 
-enregistrerImage = Blueprint('enregistrerImage', __name__)
+recuperationImage = Blueprint('recuperationImage', __name__)
 
-@enregistrerImage.route('/enregistrerImage', methods=['POST'])
-def enregistrer_image():
+@recuperationImage.route('/recuperationImage', methods=['GET'])
+def recuperation_image():
     try:
-        if 'image' not in request.files:
-            return jsonify({'message': 'Aucune image trouvée dans la requête'}), 400
-
-        image = request.files['image']
-        id_value = request.form.get('id')
-        collection_value = request.form.get('collection')
-
+        image_id = request.args.get('imageId', None)
         app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority'
         mongo = MongoClient(app.config['MONGO_URI'])
         db = mongo["test"]
         fs = gridfs.GridFS(db)
 
-        # Ajouter les champs 'id' et 'collection' en tant que métadonnées
-        metadata = {'id': id_value, 'collection': collection_value}
+        image = fs.get(ObjectId(image_id))
+        if image is None:
+            return jsonify({'message': 'Image non trouvée'}), 404
 
-        image_id = fs.put(image.stream, filename=image.filename, metadata=metadata)
-
-        return jsonify({'message': 'Image enregistrée avec succès', 'image_id': str(image_id)})
+        return send_file(io.BytesIO(image.read()), mimetype='image/jpeg')
     except Exception as e:
         current_app.logger.error(f"Error occurred: {e}")
         return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
