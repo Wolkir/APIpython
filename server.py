838f79dab6ff89070eed7208c309b90f7f278c3e
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-11 21:40:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -104,7 +104,7 @@ from routes.journal.recuperationImage import recuperationImage
 #journal
 from routes.journal.modificationTrade import setup_modificationTrade_routes
 from routes.journal.recuperationTrade import setup_things_routes
-#from routes.journal.supprimerTrade import suppressionTrade
+from routes.journal.suppressionTrade import delete_suppressionTrade
 from routes.journal.recuperationUnTrade import recuperation_un_seul_trade
 from routes.journal.correctionTrade import set_correctionTrade
 
@@ -190,7 +190,7 @@ app.register_blueprint(setup_things_routes(app))
 app.register_blueprint(setup_modificationTrade_routes(app))
 app.register_blueprint(recuperation_un_seul_trade(app))
 app.register_blueprint(set_correctionTrade(app))
-#app.register_blueprint(suppressionTrade)
+app.register_blueprint(delete_suppressionTrade(app))
 
 # image
 app.register_blueprint(enregistrerImage)
