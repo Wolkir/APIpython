2c46a67921b9b7c7502048cca2b55571f2073f3b
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-12 09:57:49 +0100

    Update enregistrementImage.py

--- routes/journal/enregistrementImage.py
+++ routes/journal/enregistrementImage.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 from bson import ObjectId
 import gridfs
 import io
-import json
 
 app = Flask(__name__)
 
@@ -16,18 +15,18 @@ def enregistrer_image():
             return jsonify({'message': 'Aucune image trouvée dans la requête'}), 400
 
         image = request.files['image']
-        id_value = str(request.form.get('id'))  # Convert the value to string
-        collection_value = str(request.form.get('collection'))  # Convert the value to string
+        id_value = request.form.get('id')
+        collection_value = request.form.get('collection')
 
         app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority'
         mongo = MongoClient(app.config['MONGO_URI'])
         db = mongo["test"]
         fs = gridfs.GridFS(db)
 
+        # Ajouter les champs 'id' et 'collection' en tant que métadonnées
         metadata = {'id': id_value, 'collection': collection_value}
-        #metadata_json = json.dumps(metadata)
 
-        image_id = fs.put(image.stream, filename=image.filename, metadata=metadata_json)
+        image_id = fs.put(image.stream, filename=image.filename, metadata=metadata)
 
         return jsonify({'message': 'Image enregistrée avec succès', 'image_id': str(image_id)})
     except Exception as e:
