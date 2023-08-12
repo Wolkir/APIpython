cb255c0d82b12724cbb9abd1909cc21c06b52ea5
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-12 21:59:16 +0100

    Update recuperationSeulRemplissage.py

--- routes/journal/recuperationSeulRemplissage.py
+++ routes/journal/recuperationSeulRemplissage.py
@@ -17,7 +17,7 @@ def get_recuperationSeulRemplissage():
         collection_brut = 'remplissageDefaut'
         collection = mongo.db[collection_brut]
 
-        remplissageDefaut = list(collection.find_one({"username": username, "nomRemplissageDefaut": nomRemplissageDefaut}))
+        remplissageDefaut = list(collection.find({"username": username}, {"nomRemplissageDefaut": nomRemplissageDefaut}))
         for strategy in remplissageDefaut:
             strategy['_id'] = str(strategy['_id'])
 
