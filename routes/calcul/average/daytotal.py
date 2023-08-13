6e9ab5219da3d74f490b303c8d11482284189d45
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-13 14:17:28 +0200

    Update daytotal.py

--- routes/calcul/average/daytotal.py
+++ routes/calcul/average/daytotal.py
@@ -64,7 +64,7 @@ def calculate_daycount(data):
         return jsonify({"distinctDateCount": distinct_count})
    
     return jsonify({"error": "No distinct dates found"}), 400
-   @daytotal.route('/average_trades_per_day', methods=['POST'])
+)
 
 @daytotal.route('/averagedaytrade', methods=['POST'])
 def calculate_averagedaytrade(data):
