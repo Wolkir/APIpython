9a903450e7892ec45120c679e1ca019a9daba2ef
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-14 00:15:24 +0200

    Create mainasset.py

---
+++ routes/calcul/average/mainasset.py
@@ -0,0 +1,53 @@
+from flask import Flask, Blueprint, jsonify, request
+from pymongo import MongoClient
+
+mainasset = Blueprint('mainasset', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+@mainasset.route('/most_common_asset', methods=['GET'])
+def most_common_asset(data):
+    # Récupérer le username depuis les paramètres de la requête
+    username = data.get('username')
+    if not username:
+        return jsonify({"error": "Username parameter is missing."}), 400
+
+    # Définir les collections en fonction du username
+    collection_name = f"{username}_close"
+    collection = db[collection_name]
+    
+    collection_unit = f"{username}_unitaire"
+    unitaire_collection = db[collection_unit]
+
+    # Utiliser une opération d'agrégation pour compter la fréquence de chaque actif
+    aggregation = [
+        {
+            "$group": {
+                "_id": "$symbol",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 1
+        }
+    ]
+    
+    most_common = list(collection.aggregate(aggregation))
+    
+    if most_common:
+        result = {
+            "most_common_asset": most_common[0]["_id"],
+            "count": most_common[0]["count"]
+        }
+        
+        # Sauvegarde de l'actif principal dans la collection unitaire
+        unitaire_collection.insert_one(result)
+        
+        return jsonify(result)
+    else:
+        return jsonify({"error": "No assets found in the database."}), 404
