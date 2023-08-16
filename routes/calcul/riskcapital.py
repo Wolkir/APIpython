7a68ce7c89dcf93a9ee9263a624249416a07b5aa
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-16 23:03:42 +0200

    Update riskcapital.py

--- routes/calcul/riskcapital.py
+++ routes/calcul/riskcapital.py
@@ -1,1 +1,21 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
 
+risk = Blueprint('risk', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+
+@risk.route('/risk', methods=['GET'])
+def calculate_risk(data):
+ 
+    point = data.get('point')
+    tick= data.get('tick')
+    SL = data.get('stopLoss')
+    Entry = data.get('priceOpening')
+    Volume = data.get('volume')
+
+    capitalrisk = (Entry - SL)*Volume*tick*(1/point)
+    return capitalrisk
