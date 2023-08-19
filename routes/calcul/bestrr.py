9bc97c261f6f13f8df468b42878017d0089ee049
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-20 00:31:17 +0200

    Update bestrr.py

--- routes/calcul/bestrr.py
+++ routes/calcul/bestrr.py
@@ -17,36 +17,43 @@ def calculate_best_rr():
     collection = db[collection_name]
 
     def get_best_for_key(key):
-        count_by_key = {}
-        total_by_key = {}
-        first_value = collection.find_one().get(meilleur, None)
-        
-        if isinstance(first_value, bool):  # Si la première valeur est un booléen
-            for doc in collection.find():
-                value = doc.get(meilleur, False)
-                key_value = doc.get(key)
-                if value:
-                    count_by_key[key_value] = count_by_key.get(key_value, 0) + 1
-            max_count = max(count_by_key.values(), default=0)
-            best_key_value = next((k for k, v in count_by_key.items() if v == max_count), None)
-            return best_key_value, max_count
-        
-        else:  # Sinon, on suppose que c'est une valeur numérique et on calcule une moyenne
-            for doc in collection.find():
-                value = doc.get(meilleur, 0)
-                key_value = doc.get(key)
-                total_by_key[key_value] = total_by_key.get(key_value, 0) + value
+    count_by_key = {}
+    total_by_key = {}
+    first_value = collection.find_one().get(meilleur, None)
+    
+    if isinstance(first_value, bool):  # Si la première valeur est un booléen
+        for doc in collection.find():
+            value = doc.get(meilleur, False)
+            key_value = doc.get(key)
+            if value:
                 count_by_key[key_value] = count_by_key.get(key_value, 0) + 1
-            
-            best_avg = -float("inf")
-            best_key_value = None
-            for key_value, total in total_by_key.items():
-                avg = total / count_by_key[key_value]
-                if avg > best_avg:
-                    best_avg = avg
-                    best_key_value = key_value
-            return best_key_value, best_avg
-
+                
+        max_count = -1
+        best_key_value = None
+        for key, count in count_by_key.items():
+            if count > max_count:
+                max_count = count
+                best_key_value = key
+                
+        return best_key_value, max_count  # on retourne le nombre de `True` pour la meilleure clé
+    
+    else:  # Sinon, on suppose que c'est une valeur numérique et on calcule une moyenne
+        for doc in collection.find():
+            value = doc.get(meilleur, 0)
+            key_value = doc.get(key)
+            total_by_key[key_value] = total_by_key.get(key_value, 0) + value
+            count_by_key[key_value] = count_by_key.get(key_value, 0) + 1
+        
+        best_avg = -float("inf")
+        best_key_value = None
+        for key_value, total in total_by_key.items():
+            avg = total / count_by_key[key_value]
+            if avg > best_avg:
+                best_avg = avg
+                best_key_value = key_value
+                
+        return best_key_value, best_avg 
+        
     best_day, best_day_value = get_best_for_key("Day")
     best_session, best_session_value = get_best_for_key("session")
     best_day,best_day_value=get_best_for_key("Day")
