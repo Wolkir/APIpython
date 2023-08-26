f2ab406058b2e0a1749aa4a253087a262fae786e
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-26 23:40:02 +0200

    Update pfreal.py

--- routes/calcul/profit/pfreal.py
+++ routes/calcul/profit/pfreal.py
@@ -4,14 +4,14 @@ from pymongo import MongoClient
 pfreal = Blueprint('pfreal', __name__)
 
 # Connexion à la base de données MongoDB
-client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://[YOUR_USERNAME]:[YOUR_PASSWORD]@[YOUR_CONNECTION_STRING]')
 db = client['test']
 
-def calculate_pfreal(data):
+def calculate_pfreal(transactions):
     gains_pondere = 0
     pertes_pondere = 0
 
-    for trans in data.get('transactions', []):
+    for trans in transactions:
         profit = trans.get('profit', 0)
         percent = trans.get('percent', 1)
         if profit > 0:
@@ -24,21 +24,24 @@ def calculate_pfreal(data):
     return gains_pondere / pertes_pondere
 
 @pfreal.route('/profit-factor-real', methods=['POST'])
-def get_profit_factor_real():
-    data = request.json
+def get_profit_factor_real(data):
+   
     username = data.get('username')
-    
+
     # Si le nom d'utilisateur n'est pas fourni, retourner une erreur
     if not username:
         return jsonify({"error": "Le champ 'username' est requis"}), 400
 
+    # Récupération des transactions de la collection appropriée
+    close_collection_name = f"{username}_close"
+    transactions = list(db[close_collection_name].find())
+
     # Calculer le Profit Factor Réel
-    result = calculate_pfreal(data)
+    result = calculate_pfreal(transactions)
 
     # Mettre à jour ou insérer dans la base de données
-    collection_name = f"{username}_unitaire"
-    user_collection = db[collection_name]
-    user_collection.update_one(
+    unitaire_collection_name = f"{username}_unitaire"
+    db[unitaire_collection_name].update_one(
         {"username": username},
         {"$set": {"profit_factor_real": result}},
         upsert=True
@@ -52,3 +55,7 @@ app.register_blueprint(pfreal)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
