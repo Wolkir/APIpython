6cdab96df18f3d6616ca144d124415e9e18d2d73
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-26 14:01:51 +0200

    Create overtrade.py

---
+++ routes/calcul/overtrade.py
@@ -0,0 +1,28 @@
+from flask import Flask, Blueprint, jsonify, request
+overtrade = Blueprint('overtrade', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+@overtrade.route('/overtrade', methods=['GET'])
+def find_overtrade(data):
+    username = request.args.get('username')
+    tradecount = request.args.get('tradecount', type=int)
+    
+    if not username or tradecount is None:
+        return "Invalid Input", 400
+
+    if tradecount > 5:
+        overtrade = True
+    else:
+        overtrade = False
+
+    return overtrade
+
+
+
+
+
+
+
