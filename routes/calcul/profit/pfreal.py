7cf9a70183daf76e9fc698a0c053f705c05d8de6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-27 00:31:14 +0200

    Update pfreal.py

--- routes/calcul/profit/pfreal.py
+++ routes/calcul/profit/pfreal.py
@@ -24,8 +24,8 @@ def calculate_pfreal(data):
     trades = user_collection.find({})
 
     for trade in trades:
-        profit = trans.get('profit', 0)
-        percent = trans.get('Percent', 1)  # si percent n'est pas présent, considérez-le comme 1 par défaut
+        profit = trade.get('profit', 0)
+        percent = trade.get('Percent', 1)  # si percent n'est pas présent, considérez-le comme 1 par défaut
 
         # Calculez le profit ou la perte pondérée basé sur le risque standard de 1%
         if profit > 0:
