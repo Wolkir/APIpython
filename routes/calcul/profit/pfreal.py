2576f909294f725f321cc111196154975d82991b
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-27 00:12:55 +0200

    Update pfreal.py

--- routes/calcul/profit/pfreal.py
+++ routes/calcul/profit/pfreal.py
@@ -7,8 +7,8 @@ pfreal = Blueprint('pfreal', __name__)
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
 
-pfreal.route('/calculer_profit_factor_reel', methods=['POST'])
-def calculate_pfreal(data):
+@pfreal.route('/calculer_profit_factor_reel', methods=['POST'])
+def calculer_profit_factor_reel(data):
     # Récupération des données du POST request
     data = request.get_json()
     username = data.get('username')
@@ -17,7 +17,7 @@ def calculate_pfreal(data):
     user_collection = db[username + '_close']
 
     # Initialisation des totaux pondérés
-    total_profit_pondere = 0
+    total_gain_pondere = 0
     total_perte_pondere = 0
 
     # Recherche de tous les trades pour cet utilisateur
