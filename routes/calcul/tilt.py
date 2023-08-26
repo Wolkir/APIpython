6b04799ec8a7809c080e9a3f521993689248bd19
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-26 13:08:52 +0200

    Create tilt.py

---
+++ routes/calcul/tilt.py
@@ -0,0 +1,40 @@
+from flask import Flask, Blueprint, jsonify
+from pymongo import MongoClient
+
+from datetime import datetime
+
+tilt_blueprint = Blueprint('tilt', __name__)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+def find_tilt(data):
+    username = data.get('username')
+    equity = data.get('equity')
+    date = data.get('dateAndTimeOpening') # Je suppose que la date est aussi fournie
+
+    if not all([username, equity, date]):
+        return "Missing data", 400
+
+    # Convertir la date reçue en objet datetime pour extraire seulement la partie jour
+    date_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f%z')
+    current_date = date_obj.strftime('%Y-%m-%d')
+
+    collection_name = f"{username}_temporaire"
+    collection = db[collection_name]
+
+    # Récupérer la valeur de `balanceopen` pour la journée
+    balance_record = collection.find_one({'date': current_date})
+    if not balance_record:
+        return "Opening balance not found", 400
+    
+    opening_balance = balance_record.get('balanceopen')
+
+    # Calculer le pourcentage de changement
+    percent_change = ((equity - opening_balance) / opening_balance) * 100
+
+    # Vérifier si le changement est inférieur à -2%
+    tilt_status = percent_change < -2
+
+    return tilt_status
