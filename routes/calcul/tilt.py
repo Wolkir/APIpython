15140b06f8988c30111bacb97288309f109cc650
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-26 13:43:51 +0200

    Update tilt.py

--- routes/calcul/tilt.py
+++ routes/calcul/tilt.py
@@ -32,10 +32,10 @@ def find_tilt(data):
     opening_balance = balance_record.get('balanceopen')
 
     # Calculer le pourcentage de changement
-    percent_change = ((equity - opening_balance) / opening_balance) * 100
+    percent_change = (equity / opening_balance) * 100
 
     # Vérifier si le changement est inférieur à -2% et renvoyer True ou False
-    if percent_change < -2:
+    if percent_change > 2:
         tilt_status = True
     else:
         tilt_status = False
