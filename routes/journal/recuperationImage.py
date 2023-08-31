95bd57540e3d041f08a1bd6a76307db2e9958b37
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-08-31 01:33:59 +0100

    Update recuperationImage.py

--- routes/journal/recuperationImage.py
+++ routes/journal/recuperationImage.py
@@ -21,15 +21,26 @@ def recuperation_image():
             return jsonify({'message': 'Aucune image trouvée'}), 404
 
         image_data = []
+        image_info_list = []
+
         for image in images:
             image_data.append(io.BytesIO(image.read()))
+
+            # Assuming you have metadata associated with the image
+            image_info = {
+                'id': image.metadata['id'],
+                'filename': image.metadata['filename'],
+                # Add other image metadata fields here
+            }
+            image_info_list.append(image_info)
 
         response = app.response_class(
             response=image_data[0].getvalue(),
             content_type='image/jpeg'
         )
 
-        return response
+        return jsonify({'image_info': image_info_list, 'image_data': response.data})
     except Exception as e:
         current_app.logger.error(f"Error occurred: {e}")
         return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
+
