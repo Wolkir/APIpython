5cf0da8b7c50d4f4e3d06f88139147ed1c959300
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-05 01:07:24 +0100

    Update recuperationTrade.py

--- routes/journal/recuperationTrade.py
+++ routes/journal/recuperationTrade.py
@@ -58,34 +58,4 @@ def setup_things_routes(app):
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-
-    @things_blueprint.route('/suppressionTrade', methods=['DELETE'])
-    def delete_suppressionTrade():
-        try:
-            data = request.json
-            collection = data.get('collection')
-            id_str = data.get('id')
-    
-            if not collection or not id_str:
-                return jsonify({"error": "Le nom de la collection ou l'id sont manquants dans la requête"}), 400
-    
-            try:
-                id = ObjectId(id_str)
-            except:
-                return jsonify({"error": "L'ID fourni n'est pas valide"}), 400
-    
-            mongo = PyMongo(current_app)
-            collectionDefinitive = mongo.db[collection]
-    
-            result = collectionDefinitive.delete_one({"_id": id})
-    
-            if result.deleted_count > 0:
-                return jsonify({"message": "Trade supprimé avec succès"}), 200
-            else:
-                return jsonify({"error": "Le trade n'a pas été trouvé"}), 500
-    
-        except Exception as e:
-            current_app.logger.error(f"Error occurred: {e}")
-            return jsonify({"error": "Erreur lors de la suppression du trade", "details": str(e)}), 500
-        
     return things_blueprint
