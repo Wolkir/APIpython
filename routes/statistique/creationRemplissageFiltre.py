648ee6e068d504f2d0937971a750204687dfb975
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-09 13:45:33 +0100

    Update creationRemplissageFiltre.py

--- routes/statistique/creationRemplissageFiltre.py
+++ routes/statistique/creationRemplissageFiltre.py
@@ -23,11 +23,25 @@ def creation_remplissage_filtre():
                         document_mongodb[key] = []
                     document_mongodb[key].append(value)
 
+            # Vérifiez si les champs 'string' de 'nomRemplissage' ne sont pas déjà présents dans la collection
+            if 'nomRemplissage' in document_mongodb:
+                existing_documents = collection.find({'nomRemplissage': {'$in': document_mongodb['nomRemplissage']}})
+                for existing_doc in existing_documents:
+                    for item in tableau_json:
+                        if 'nomRemplissage' in item:
+                            for value in item['nomRemplissage']:
+                                if value in existing_doc['nomRemplissage']:
+                                    return jsonify({"message": f"La valeur '{value}' existe déjà dans la collection"}), 400
+
             # Insérez le document MongoDB unique dans la collection MongoDB
             collection.insert_one(document_mongodb)
-            
+
             return jsonify({"message": "Données insérées avec succès"}), 200
         else:
             return jsonify({"message": "Le corps de la requête doit être un tableau JSON"}), 400
     except Exception as e:
         return jsonify({"message": str(e)}), 500
+
+if __name__ == '__main__':
+    app.register_blueprint(creationRemplissageFiltre)
+    app.run()
