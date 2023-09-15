af3b877a79d0bfec749ae5e733ff3bdbb26fa8a2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-15 16:16:21 +0100

    Update recuperationImage.py

--- routes/journal/recuperationImage.py
+++ routes/journal/recuperationImage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask, request, send_file, jsonify
+from flask import Blueprint, Flask, request, jsonify
 from pymongo import MongoClient
 import gridfs
 
@@ -22,21 +22,46 @@ def recuperation_images():
         if images.count() == 0:
             return jsonify({'message': 'Aucune image trouvée'}), 404
 
-        # Créez une liste pour stocker les réponses
-        responses = []
+        # Créez une liste pour stocker les données JSON des images
+        image_data_list = []
 
-        # Parcourez les images et ajoutez-les à la liste de réponses
+        # Parcourez les images et ajoutez leurs données à la liste
         for image in images:
-            response = send_file(
-                image, 
-                mimetype='image/jpeg',
-                as_attachment=True,  # Permet le téléchargement
-                attachment_filename=f'image_{image_id}.jpg'  # Nom du fichier à télécharger
-            )
-            responses.append(response)
+            image_data = {
+                'image_id': image_id,
+                'image_url': f'/image/{image_id}',  # URL pour récupérer l'image individuelle
+            }
+            image_data_list.append(image_data)
 
-        # Renvoyer la liste de réponses (toutes les images correspondantes)
-        return responses
+        # Renvoyez la liste des données JSON (toutes les images correspondantes)
+        return jsonify(image_data_list)
+
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
+
+# Ajoutez une nouvelle route pour récupérer une image individuelle par son ID
+@recuperationImage.route('/image/<image_id>', methods=['GET'])
+def recuperation_image(image_id):
+    try:
+        # Connexion à la base de données MongoDB
+        client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+        db = client["test"]
+        fs = gridfs.GridFS(db)
+
+        # Recherche de l'image par ID
+        image = fs.find_one({'metadata.id': image_id})
+
+        if image is None:
+            return jsonify({'message': 'Image non trouvée'}), 404
+
+        # Renvoie l'image sous forme de réponse de fichier
+        return send_file(
+            image,
+            mimetype='image/jpeg',
+            as_attachment=True,  # Permet le téléchargement
+            attachment_filename=f'image_{image_id}.jpg'  # Nom du fichier à télécharger
+        )
 
     except Exception as e:
         app.logger.error(f"Error occurred: {e}")
@@ -45,4 +70,5 @@ def recuperation_images():
 if __name__ == "__main__":
     app.register_blueprint(recuperationImage)
     app.run(debug=True)
+
 
