5725ee25e615156057cedd8fc1a3fc4b8fcab2f2
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-15 15:54:50 +0100

    Update recuperationImage.py

--- routes/journal/recuperationImage.py
+++ routes/journal/recuperationImage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, Flask, request
+from flask import Blueprint, Flask, request, send_file, jsonify
 from pymongo import MongoClient
 import gridfs
 
@@ -10,22 +10,31 @@ recuperationImage = Blueprint('recuperationImage', __name__)
 def recuperation_image():
     try:
         image_id = request.args.get('imageId', None)
-        app.config['MONGO_URI'] = 'mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority'
-        mongo = MongoClient(app.config['MONGO_URI'])
-        db = mongo["test"]
+
+        # Connexion à la base de données MongoDB
+        client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+        db = client["test"]
         fs = gridfs.GridFS(db)
 
-        images = fs.find({'metadata.id': image_id})
-        if images.count() == 0:
+        # Recherche de l'image par ID
+        image = fs.find_one({'metadata.id': image_id})
+
+        if image is None:
             return jsonify({'message': 'Aucune image trouvée'}), 404
 
-        image_data = []
+        # Renvoyer l'image en tant que fichier
+        response = send_file(
+            image, 
+            mimetype='image/jpeg',
+            as_attachment=True,
+            download_name=f'image_{image_id}.jpg'
+        )
+        return response
 
-        for image in images:
-            image_data.append({'_id': str(image._id), 'url': image['metadata']['url']})
-
-        return jsonify(image_data)
     except Exception as e:
         app.logger.error(f"Error occurred: {e}")
         return jsonify({'message': f'Une erreur est survenue : {str(e)}'}), 500
 
+if __name__ == "__main__":
+    app.register_blueprint(recuperationImage)
+    app.run(debug=True)
