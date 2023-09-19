3d9a149a24659bddf2408c17fb7ac371f4749582
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-19 21:37:22 +0100

    Update winrate.py

--- routes/calcul/winrate.py
+++ routes/calcul/winrate.py
@@ -10,37 +10,44 @@ db = client['test']
 
 @winrate.route('/winrate', methods=['GET'])
 def calculate_winrate():
-    data = request.json
-    username = data.get('username')
-    identifier = data.get('identifier')
-    collection_name = data.get('collection')
-    collection_unitaire = f"{username}_unitaire"
-    collection = db[collection_name]
-  
-    # Récupérer tous les documents
-    documents = list(collection.find())
-    
-    positive_profits_count = 0
-    negative_profits_count = 0
-    
-    positive_identifiers = set()
-    negative_identifiers = set()
-    
-    for doc in documents:
-        profit = doc['profit']
-        identifier = doc['identifier']
+    try:
+        data = request.json
+        username = data.get('username')
+        identifier = data.get('identifier')
+        collection_name = data.get('collection')
+        collection_unitaire = f"{username}_unitaire"
+        collection = db[collection_name]
+      
+        # Récupérer tous les documents
+        documents = list(collection.find())
         
-        if profit > 0 and identifier not in positive_identifiers:
-            positive_profits_count += 1
-            positive_identifiers.add(identifier)
-        elif profit < 0 and identifier not in negative_identifiers:
-            negative_profits_count += 1
-            negative_identifiers.add(identifier)
+        positive_profits_count = 0
+        negative_profits_count = 0
+        
+        positive_identifiers = set()
+        negative_identifiers = set()
+        
+        for doc in documents:
+            profit = doc['profit']
+            identifier = doc['identifier']
+            
+            if profit > 0 and identifier not in positive_identifiers:
+                positive_profits_count += 1
+                positive_identifiers.add(identifier)
+            elif profit < 0 and identifier not in negative_identifiers:
+                negative_profits_count += 1
+                negative_identifiers.add(identifier)
+    
+        
+        # Calcul du winrate
+        winrate_value = positive_profits_count / (positive_profits_count + negative_profits_count) * 100
+        
+        # Insérer le winrate_value dans la collection "unitaire"
+        unitaire_collection = db[collection_unitaire]
+        unitaire_collection.update_one({}, {'$set': {'winratereal': (winrate_value)}}, upsert=True)
 
-    
-    # Calcul du winrate
-    winrate_value = positive_profits_count / (positive_profits_count + negative_profits_count) * 100
-    
-    # Insérer le winrate_value dans la collection "unitaire"
-    unitaire_collection = db[collection_unitaire]
-    unitaire_collection.update_one({}, {'$set': {'winratereal': (winrate_value)}}, upsert=True)
+        return jsonify({"message": "Winrate calculé avec succès"})
+    else:
+        return jsonify({"error": "La demande ne contient pas de données JSON valide"}), 400
+except Exception as e:
+    return jsonify({"error": str(e)}), 500
