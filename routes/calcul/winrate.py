a558dbe045ff70bc7fc7bff2628287d2862e0ae4
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-09-26 13:24:39 +0100

    Update winrate.py

--- routes/calcul/winrate.py
+++ routes/calcul/winrate.py
@@ -23,7 +23,7 @@ def calculate_winrate():
         collection_name = ""
         username = ""
         filtreDeBase = ""
-        collection = db['f"utile_{username}_temporaire"'] # valeur sans consequence pour pouvoir initialiser la variable
+        collection = db[f"utile_{username}_temporaire"] # valeur sans consequence pour pouvoir initialiser la variable
         
         if isinstance(data, list):
             for item in data:
