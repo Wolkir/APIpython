53dcb46a1e922df160c2a4a977596aac40833039
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-10-06 14:10:58 +0100

    Update envoie.py

--- routes/envoie/envoie.py
+++ routes/envoie/envoie.py
@@ -18,6 +18,16 @@ from .XY import process_argument_xyTilt
 from .XY import process_argument_xyAnnEco
 from .XY import process_argument_xyVioleStrat
 from .XY import process_argument_xyTJS
+from .calcul.maxSuccessiveGain import maxSuccessiveGain
+from .calcul.maxSuccessiveLoss import maxSuccessiveLoss
+from .calcul.maxProfit import maxProfit
+from .calcul.profitFactorGroup import profitFactorGroup
+from .calcul.winrateGroup import winrateGroup
+from .calcul.totalTrade import totalTrade
+from .calcul.RRaverage import RRaverage
+from .calcul.averageDuration import averageDuration 
+from .calcul.averageGain import averageGain
+from .calcul.maxProfitMinLoss import maxProfitMinLoss
 
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 envoie = Blueprint('envoie', __name__)
@@ -30,7 +40,7 @@ def json_serial(obj):
         if isinstance(obj, datetime):
             return obj.isoformat()
         if isinstance(obj, bytes):
-            return obj.decode('utf-8')  # Convertir les bytes en chaînes
+            return obj.decode('utf-8')
         raise TypeError("Type not serializable")
     except Exception as e:
         print("Error occurred during serialization:")
@@ -57,7 +67,7 @@ def update_envoie():
     argI = process_argument_value(request.args.get('argI', None))
     debutDate = request.args.get('argSD', None)
     finDate = request.args.get('argED', None)
-    # username = request.args.get('username', None)
+    username = request.args.get('username', None)
     argTPR = process_argument_value(request.args.get('argTPR', None))
     argSL = process_argument_value(request.args.get('argSL', None))
     argBE = process_argument_value(request.args.get('argBE', None))
@@ -81,6 +91,7 @@ def update_envoie():
 
     db = client['test']
     collection = db[argCollection]
+    collection_unitaire = f'{username}_unitaire'
 
     start_date, end_date = process_argument_date(argD, debutDate, finDate)
 
@@ -205,8 +216,49 @@ def update_envoie():
         else:
             query = {}
             data = list(collection.find(query))
+
+
+
+        # ==================== RECUPERATION TRADES TERMINEE ==================== # maxProfit
+
+
+
+        max_successive_gains_count = maxSuccessiveGain(data, collection_unitaire)
+        max_successive_losses_count = maxSuccessiveLoss(data, collection_unitaire)
+        profit_factor, profit_factor_buy, profit_factor_sell, total_profit, total_loss, total_profit_buy, total_loss_buy, total_profit_sell, total_loss_sell = profitFactorGroup(data, collection_unitaire)
+        winratestd, winrate_value_real, winratelongstd, winrateshortstd = winrateGroup(data, collection_unitaire)
+        average_rr = RRaverage(data, collection_unitaire)
+        average_duration = averageDuration(data, collection_unitaire)
+        average_gain = averageGain(data, collection_unitaire)
+        #total_trade = totalTrade(data, collection_unitaire)
+        sharpe_ratio, max_profit_value, min_loss_value, max_equity = maxProfitMinLoss(data, collection_unitaire)
+        print("max_successive_gains_count : ", max_successive_gains_count)
+        print("max_successive_losses_count : ", max_successive_losses_count)
+        print("profit_factor : ", profit_factor)
+        print("profit_factor_buy : ", profit_factor_buy)
+        print("profit_factor_sell : ", profit_factor_sell)
+        print("total_profit : ", total_profit)
+        print("total_loss : ", total_loss)
+        print("total_profit_buy : ", total_profit_buy)
+        print("total_loss_buy : ", total_loss_buy)
+        print("total_profit_sell : ", total_profit_sell)
+        print("total_loss_sell : ", total_loss_sell)
+        print("winratestd : ", winratestd)
+        print("winrate_value_real : ", winrate_value_real)
+        print("winratelongstd : ", winratelongstd)
+        print("winrateshortstd : ", winrateshortstd)
+        print("average_rr : ", average_rr)
+        print("average_duration : ", average_duration)
+        print("average_gain : ", average_gain)
+        print("sharpe_ratio : ", sharpe_ratio)
+        print("max_profit_value : ", max_profit_value)
+        print("min_loss_value : ", min_loss_value)
+        print("max_equity : ", max_equity)
+        #print("total_trade : ", total_trade)
+
         data = json.loads(json.dumps(data, default=json_serial))
         return jsonify({'data': data})
     except Exception as e:
         current_app.logger.error(f"Error occurred: {e}")
-        return jsonify({"error": "Erreur lors de l'enregistrement des stratégies pour l'utilisateur donné", "details": str(e)}), 500
+        return jsonify({"error": "Erreur lors du renvoie des données", "details": str(e)}), 500
+
