8514fd79599e2f59ef522b59faf4017b6a855e22
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-10-18 16:22:02 +0100

    Update enregistrerNote.py

--- routes/note/enregistrerNote.py
+++ routes/note/enregistrerNote.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, request, jsonify
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
 db = client['test']
