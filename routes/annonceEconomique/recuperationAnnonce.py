f47da8689ab45fdfca9acabc9a596aeb2606f749
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-10-19 08:46:40 +0100

    Update recuperationAnnonce.py

--- routes/annonceEconomique/recuperationAnnonce.py
+++ routes/annonceEconomique/recuperationAnnonce.py
@@ -1,14 +1,18 @@
-from flask import Flask, Blueprint, current_app
+from flask import Flask, Blueprint, current_app, jsonify
 import schedule
 import time
+import threading
 import requests
 
-recuperationAnnonce = Blueprint('recuperationAnnonce', __name__)
+app = Flask(__name)
+
+recuperationAnnonce = Blueprint('recuperationAnnonce', __name)
 
 def get_data_from_api():
     api_url = 'https://eodhd.com/api/economic-events?api_token=6530da188309e6.66620223'
     data = requests.get(api_url).json()
     print(data)
+    return data
 
 def schedule_weekly_task():
     schedule.every().monday.at("10:00").do(get_data_from_api)
@@ -18,12 +22,20 @@ def run_schedule():
         schedule.run_pending()
         time.sleep(1)
 
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    data = get_data_from_api()
+    return jsonify(data)
+
 @recuperationAnnonce.record
 def setup_schedule(state):
     app = state.app
     if app:
         schedule_weekly_task()
-        import threading
         t = threading.Thread(target=run_schedule)
         t.daemon = True
         t.start()
+
+if __name__ == '__main__':
+    app.register_blueprint(recuperationAnnonce)
+    app.run()
