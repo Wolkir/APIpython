2f1ef583d710be39ce6c42276aeebe7c0f97966c
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-11-02 15:18:25 +0100

    Create date.py

---
+++ routes/graphique/general/date.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+def process_argument_date(debutDate, finDate):
+    date_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+    try:
+        start_date = datetime.strptime(debutDate, date_format)
+        finDate_cleaned = finDate.strip()
+        print("finDate_cleaned:", finDate_cleaned)
+        end_date = datetime.strptime(finDate_cleaned, date_format)
+        return start_date, end_date
+    except ValueError as e:
+        print("Erreur lors de la conversion des dates:", e)
+        return None, None
+    return None, None
+
+
