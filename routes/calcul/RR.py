05ae924da76cffea1a6ddf061c9b0934a9b29601
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-11-15 18:37:28 +0100

    Create RR.py

---
+++ routes/calcul/RR.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from pymongo import MongoClient
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://pierre:ztxiGZypi6BGDMSY@atlascluster.sbpp5xm.mongodb.net/?retryWrites=true&w=majority')
+db = client['test']
+
+RR = Blueprint('RR', __name__)
+@RR.route('/rr', methods=['GET'])
+
+def calculate_rr(data):
+  
+    price_close = data['priceClosure']
+    price_opening = data['priceOpening']
+    stop_loss = data['stopLoss']
+    rr = (price_close - price_opening) / (price_opening - stop_loss) if (price_opening - stop_loss) > 0 else 0
+    rr = round(rr, 2)
+    return rr  # Renvoie la valeur de la clé "RR"
