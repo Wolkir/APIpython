94450afe011c70a4cc2f588bb76884508bca0a2f
guillaumepons <helyomsgaming@gmail.com>
2023-11-29 13:19:12 +0100

    Fix du changement de position (MQL4 et MQL5)

--- connexion/tradReq/tradReq.py
+++ connexion/tradReq/tradReq.py
@@ -122,8 +122,7 @@ def save_trade_request():
             open_orders.update_one({"identifier": data.get('identifier')}, {"$set": {"RRT": data.get('RRT')}})
 
         if closure_position == "Open" and data.get('typeOfTransaction') != "ModifySl":
-            volume_remain = data.get('volume')
-            #return jsonify({"message", str(data.get('volume'))}), 300
+            volume_remain = float(data.get('volume'))
             if volume_remain < 0.01:
                 volume_remain = 0
                 user_collection.delete_one({"identifier": data.get('identifier')})
@@ -137,8 +136,7 @@ def save_trade_request():
             open_orders = db[f"{username}_open"]
             open_order = open_orders.find_one({"identifier": data.get('identifier')})
             if open_order:
-                volume_remain = open_order.get('volume_remain', 0) - data.get('volume')
-                #return jsonify({"message: ", "volume_remain: "+str(open_order.get('volume_remain', 0))+" --- float volume: "+str(float(data.get('volume')))}), 300
+                volume_remain = open_order.get('volume_remain', 0) - float(data.get('volume'))
                 if volume_remain < 0:
                     volume_remain = 0
                 open_orders.update_one({"identifier": data.get('identifier')}, {"$set": {"volume_remain": volume_remain}})
@@ -149,7 +147,7 @@ def save_trade_request():
         
         
         if closure_position == "ModiySl":
-            data['volume'] = round(data.get('volume'), 2)
+            data['volume'] = round(float(data.get('volume')), 2)
             volume_remain = round(volume_remain, 2)
             
 
